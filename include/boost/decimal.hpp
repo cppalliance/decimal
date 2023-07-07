503006fa9c1c892398908d73c760fcde5822cd3c
Matt Borland <matt@mattborland.com>
2023-07-07 11:40:00 +0200

    Add project structure

---
+++ include/boost/decimal.hpp
@@ -0,0 +1,10 @@
+// Copyright 2023 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_DECIMAL_HPP
+#define BOOST_DECIMAL_HPP
+
+#include <boost/decimal/decimal32.hpp>
+
+#endif //BOOST_DECIMAL_HPP
---
+++ include/boost/decimal/decimal32.hpp
@@ -0,0 +1,10 @@
+// Copyright 2023 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_DECIMAL_DECIMAL32_HPP
+#define BOOST_DECIMAL_DECIMAL32_HPP
+
+#include <boost/decimal/config.hpp>
+
+#endif // BOOST_DECIMAL_DECIMAL32_HPP
