1ad1333ec559a6bc8c45d0a1b11da9b4fa2af8ad
Matt Borland <matt@mattborland.com>
2023-08-01 15:17:59 -0400

    Add values from table 3.6

--- src/decimal32.cpp
+++ src/decimal32.cpp
@@ -2,4 +2,19 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/decimal/decimal32.cpp>
+#include <boost/decimal/decimal32.hpp>
+
+namespace boost { namespace decimal {
+
+namespace detail {
+
+// Values from IEEE 754-2019 table 3.6
+BOOST_ATTRIBUTE_UNUSED static constexpr auto storage_width = 32;
+BOOST_ATTRIBUTE_UNUSED static constexpr auto precision = 7;
+BOOST_ATTRIBUTE_UNUSED static constexpr auto emax = 96;
+BOOST_ATTRIBUTE_UNUSED static constexpr auto bias = 101;
+BOOST_ATTRIBUTE_UNUSED static constexpr auto combination_field_width = 11;
+BOOST_ATTRIBUTE_UNUSED static constexpr auto trailing_significand_field_width = 20;
+
+} // Namespace detail
+}} // Namespace boost::decimal
