6e143d39048dec4a5437c93bd147d86e81319b1b
Matt Borland <matt@mattborland.com>
2023-08-03 11:57:43 -0400

    Add masks for the combination field

--- src/decimal32.cpp
+++ src/decimal32.cpp
@@ -36,6 +36,32 @@ BOOST_ATTRIBUTE_UNUSED static constexpr std::uint32_t g2_mask = 0b00100;
 BOOST_ATTRIBUTE_UNUSED static constexpr std::uint32_t g3_mask = 0b00010;
 BOOST_ATTRIBUTE_UNUSED static constexpr std::uint32_t g4_mask = 0b00001;
 
+// Masks to update the significand based on the combination field
+// In these first three 00, 01, or 10 are the leading 2 bits of the exp
+// and the trailing 3 bits are to be concatenated onto the significand (23 bits total)
+//
+//    Comb.  Exponent          Significand
+// s 00 TTT (00)eeeeee (0TTT)[tttttttttt][tttttttttt]
+// s 01 TTT (01)eeeeee (0TTT)[tttttttttt][tttttttttt]
+// s 10 TTT (10)eeeeee (0TTT)[tttttttttt][tttttttttt]
+BOOST_ATTRIBUTE_UNUSED static constexpr std::uint32_t comb_00_mask = 0b00000;
+BOOST_ATTRIBUTE_UNUSED static constexpr std::uint32_t comb_01_mask = 0b01000;
+BOOST_ATTRIBUTE_UNUSED static constexpr std::uint32_t comb_10_mask = 0b10000;
+
+// This mask is used to determine if we use the masks above or below since 11 TTT is invalid
+BOOST_ATTRIBUTE_UNUSED static constexpr std::uint32_t comb_11_mask = 0b11000;
+
+// For these masks the first two bits of the combination field imply 100 T as the
+// leading bits of the significand and then bits 3 and 4 are the exp
+//
+//    Comb.  Exponent          Significand
+// s 1100 T (00)eeeeee (100T)[tttttttttt][tttttttttt]
+// s 1101 T (01)eeeeee (100T)[tttttttttt][tttttttttt]
+// s 1110 T (10)eeeeee (100T)[tttttttttt][tttttttttt]
+BOOST_ATTRIBUTE_UNUSED static constexpr std::uint32_t comb_1100_mask = 0b11000;
+BOOST_ATTRIBUTE_UNUSED static constexpr std::uint32_t comb_1101_mask = 0b11010;
+BOOST_ATTRIBUTE_UNUSED static constexpr std::uint32_t comb_1110_mask = 0b11100;
+
 } // Namespace detail
 
 decimal32::decimal32(long long coeff, int exp) noexcept
