1d755c4003c62340c51bd4198769266f923a4a84
Matt Borland <matt@mattborland.com>
2023-08-09 10:26:40 -0400

    Add endianness detection

--- include/boost/decimal/detail/config.hpp
+++ include/boost/decimal/detail/config.hpp
@@ -7,4 +7,21 @@
 
 #include <boost/config.hpp>
 
+// Determine endianness
+#if defined(_WIN32)
+
+#define BOOST_DECIMAL_ENDIAN_BIG_BYTE 0
+#define BOOST_DECIMAL_ENDIAN_LITTLE_BYTE 1
+
+#elif defined(__BYTE_ORDER__)
+
+#define BOOST_DECIMAL_ENDIAN_BIG_BYTE (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+#define BOOST_DECIMAL_ENDIAN_LITTLE_BYTE (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+
+#else
+
+#error Could not determine endian type. Please file an issue at https://github.com/cppalliance/decimal with your architecture
+
+#endif // Determine endianness
+
 #endif // BOOST_DECIMAL_DETAIL_CONFIG_HPP
