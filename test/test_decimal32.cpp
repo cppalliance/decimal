21caea3f6cbe0bd5e3c34aed99ce138017b37487
Matt Borland <matt@mattborland.com>
2023-08-09 10:16:16 -0400

    Add constructor tests

--- test/test_decimal32.cpp
+++ test/test_decimal32.cpp
@@ -4,19 +4,40 @@
 
 #include <boost/decimal/decimal32.hpp>
 #include <boost/core/lightweight_test.hpp>
-#include <cassert>
+#include <bitset>
 
-int main()
+void test_comp()
 {
     boost::decimal::decimal32 small(1, -50);
     BOOST_TEST(small == small);
 
-    std::cerr << small << std::endl;
-
     boost::decimal::decimal32 sig(123456, -50);
     BOOST_TEST(sig != small);
+}
 
-    std::cerr << sig << std::endl;
+void test_constructor()
+{
+    boost::decimal::decimal32 one(0b1, -100);
+    const auto one_bits = std::bitset<32>(to_bits(one));
+    // 0 for sign
+    // 00000 for combination field
+    // 000001 for exp
+    // 0000000000'0000000001 for significand
+    BOOST_TEST_EQ(one_bits, std::bitset<32>(0b0'00000'000001'0000000000'0000000001));
+
+    boost::decimal::decimal32 big_sig(0b1111111111'1111111111, -100);
+    const auto big_sig_bits = std::bitset<32>(to_bits(big_sig));
+    // 0 for sign
+    // 00000 for combination field
+    // 000001 for exp
+    // 1111111111'1111111111 for significand
+    BOOST_TEST_EQ(big_sig_bits, std::bitset<32>(0b0'00000'000001'1111111111'1111111111));
+}
+
+int main()
+{
+    test_comp();
+    test_constructor();
 
     return boost::report_errors();
 }
