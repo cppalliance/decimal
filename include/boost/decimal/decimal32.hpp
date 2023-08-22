27a8de24ce66e1055b5d543775752d1c3a792d6c
Matt Borland <matt@mattborland.com>
2023-08-22 11:19:13 -0400

    Implement operators* and *=

--- include/boost/decimal/decimal32.hpp
+++ include/boost/decimal/decimal32.hpp
@@ -195,6 +195,9 @@ public:
     constexpr decimal32 operator--(int) noexcept; // NOLINT : C++14 so constexpr implies const
     constexpr decimal32& operator-=(decimal32 rhs) noexcept;
 
+    friend constexpr decimal32 operator*(decimal32 lhs, decimal32 rhs) noexcept;
+    constexpr decimal32& operator*=(decimal32 rhs) noexcept;
+
     // 3.2.9 comparison operators:
     friend constexpr bool operator==(decimal32 lhs, decimal32 rhs) noexcept;
     friend constexpr bool operator!=(decimal32 lhs, decimal32 rhs) noexcept;
@@ -1035,6 +1038,47 @@ std::uint32_t to_bits(decimal32 rhs) noexcept
     std::uint32_t bits;
     std::memcpy(&bits, &rhs.bits_, sizeof(std::uint32_t));
     return bits;
+}
+
+constexpr decimal32 operator*(decimal32 lhs, decimal32 rhs) noexcept
+{
+    constexpr decimal32 zero {0, 0};
+
+    const auto res {check_non_finite(lhs, rhs)};
+    if (res != zero)
+    {
+        return res;
+    }
+
+    auto sig_lhs {lhs.full_significand()};
+    auto exp_lhs {lhs.biased_exponent()};
+    normalize(sig_lhs, exp_lhs);
+
+    auto sig_rhs {rhs.full_significand()};
+    auto exp_rhs {rhs.biased_exponent()};
+    normalize(sig_rhs, exp_rhs);
+
+    const bool sign {!(lhs.isneg() == rhs.isneg())};
+
+    // Once we have the normalized significands and exponents all we have to do is
+    // multiply the significands and add the exponents
+    //
+    // We use a 64 bit resultant significand because the two 23-bit unsigned significands will always fit
+
+    auto res_sig {static_cast<std::int64_t>(sig_lhs) * static_cast<std::int64_t>(sig_rhs)};
+    if (sign)
+    {
+        res_sig = -res_sig;
+    }
+    const auto res_exp {exp_lhs + exp_rhs};
+
+    return decimal32 {res_sig, res_exp};
+}
+
+constexpr decimal32& decimal32::operator*=(decimal32 rhs) noexcept
+{
+    *this = *this * rhs;
+    return *this;
 }
 
 }} // Namespace boost::decimal
