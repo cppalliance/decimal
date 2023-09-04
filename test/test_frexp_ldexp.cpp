5d325b4826d46e81069a56d6e083d711e0e40632
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-09-04 07:36:02 +0200

    Use C-style struct and add test near 8388606.5

--- test/test_frexp_ldexp.cpp
+++ test/test_frexp_ldexp.cpp
@@ -35,12 +35,6 @@ namespace local
     return static_cast<local_integral_time_point_type>(current_now);
   }
 
-  template<typename FloatType>
-  auto is_excluded(const FloatType x) -> bool
-  {
-    return ((x >= static_cast<FloatType>(8388606.5L)) && (x < static_cast<FloatType>(8388607.5L)));
-  }
-
   template<typename NumericType>
   auto is_close_fraction(const NumericType& a,
                          const NumericType& b,
@@ -53,19 +47,14 @@ namespace local
     return (delta < tol);
   }
 
-  struct test_frexp_ldexp_ctrl
+  typedef struct test_frexp_ldexp_ctrl
   {
-    constexpr test_frexp_ldexp_ctrl(const float lo, const float hi, const bool b, const std::uint32_t c)
-      : float_value_lo(lo),
-        float_value_hi(hi),
-        b_neg         (b),
-        count         (c) { }
-
-    const float         float_value_lo;
-    const float         float_value_hi;
-    const bool          b_neg;
-    const std::uint32_t count;
-  };
+    float         float_value_lo;
+    float         float_value_hi;
+    bool          b_neg;
+    std::uint32_t count;
+  }
+  test_frexp_ldexp_ctrl;
 
   auto test_frexp_ldexp(const test_frexp_ldexp_ctrl& ctrl) -> bool
   {
@@ -99,7 +88,7 @@ namespace local
       {
         flt_start = dis(gen);
 
-        if(flt_start != static_cast<float>(0.0L) && (!is_excluded(flt_start)))
+        if(flt_start != static_cast<float>(0.0L))
         {
           break;
         }
@@ -150,15 +139,15 @@ namespace local
 
 auto main() -> int
 {
-  constexpr auto flt_ctrl =
-    std::array<local::test_frexp_ldexp_ctrl, static_cast<std::size_t>(UINT8_C(5))>
-    {
-      local::test_frexp_ldexp_ctrl( { -1.0E7F  , +1.0E7F,  false, static_cast<std::uint32_t>(UINT32_C(0x10000)) } ),
-      local::test_frexp_ldexp_ctrl( {  1.0E-20F, +1.0E-1F, false, static_cast<std::uint32_t>(UINT32_C(0x10000)) } ),
-      local::test_frexp_ldexp_ctrl( {  1.0E-20F, +1.0E-1F, true , static_cast<std::uint32_t>(UINT32_C(0x10000)) } ),
-      local::test_frexp_ldexp_ctrl( {  10.0F   , +1.0E12F, false, static_cast<std::uint32_t>(UINT32_C(0x10000)) } ),
-      local::test_frexp_ldexp_ctrl( {  10.0F   , +1.0E12F, true , static_cast<std::uint32_t>(UINT32_C(0x10000)) } ),
-    };
+  const local::test_frexp_ldexp_ctrl flt_ctrl[static_cast<std::size_t>(UINT8_C(6))] =
+  {
+    { 8388606.5F, 8388607.5F, false, static_cast<std::uint32_t>(UINT32_C(0x100)) },
+    { -1.0E7F   , +1.0E7F,    false, static_cast<std::uint32_t>(UINT32_C(0x20000)) },
+    {  1.0E-20F , +1.0E-1F,   false, static_cast<std::uint32_t>(UINT32_C(0x20000)) },
+    {  1.0E-20F , +1.0E-1F,   true , static_cast<std::uint32_t>(UINT32_C(0x20000)) },
+    {  10.0F    , +1.0E12F,   false, static_cast<std::uint32_t>(UINT32_C(0x20000)) },
+    {  10.0F    , +1.0E12F,   true , static_cast<std::uint32_t>(UINT32_C(0x20000)) },
+  };
 
   auto result_is_ok = true;
 
