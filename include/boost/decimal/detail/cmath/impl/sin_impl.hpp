cde17bc27b36f3d4ce168177691e7bd8c2e158b0
Matt Borland <matt@mattborland.com>
2023-09-13 11:16:28 +0200

    Make sin constatns constexpr decimals

--- include/boost/decimal/detail/cmath/impl/sin_impl.hpp
+++ include/boost/decimal/detail/cmath/impl/sin_impl.hpp
@@ -20,16 +20,28 @@ constexpr auto sin_impl(T x) noexcept -> T
     {
         return -sin_impl(-x);
     }
+    
+    constexpr T a0 {UINT64_C(94465293626777997), -22};
+    constexpr T a1 {UINT64_C(2121051143997598), -19, true};
+    constexpr T a2 {UINT64_C(10766679285155848), -21};
+    constexpr T a3 {UINT64_C(83283844849017102), -19};
+    constexpr T a4 {UINT64_C(13384154242642468), -22};
+    constexpr T a5 {UINT64_C(16666687019704884), -17, true};
+    constexpr T a6 {UINT64_C(15512908179440871), -24};
+    constexpr T a7 {UINT64_C(99999999954464358), -17};
+    constexpr T a8 {UINT64_C(22004194322650135), -28};
 
-    T u = T(9.4465293626777997e-06);
-    u = u * x + T(-0.0002121051143997598);
-    u = u * x + T(1.0766679285155848e-05);
-    u = u * x + T(0.0083283844849017102);
-    u = u * x + T(1.3384154242642468e-06);
-    u = u * x + T(-0.16666687019704884);
-    u = u * x + T(1.5512908179440871e-08);
-    u = u * x + T(0.99999999954464358);
-    return u * x + T(2.2004194322650135e-12);
+    T u {a0};
+    u = fma(u, x, a1);
+    u = fma(u, x, a2);
+    u = fma(u, x, a3);
+    u = fma(u, x, a4);
+    u = fma(u, x, a5);
+    u = fma(u, x, a6);
+    u = fma(u, x, a7);
+    u = fma(u, x, a8);
+
+    return u;
 }
 
 }}} // Namespaces
