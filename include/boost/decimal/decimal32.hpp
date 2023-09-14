b8f1a7c4240b112eeb4c9136b7b3df814a8de438
Matt Borland <matt@mattborland.com>
2023-09-14 11:17:49 +0200

    Specialize operator<= for decimal32 and int

--- include/boost/decimal/decimal32.hpp
+++ include/boost/decimal/decimal32.hpp
@@ -323,6 +323,12 @@ public:
     // Less equal
     friend constexpr bool operator<=(decimal32 lhs, decimal32 rhs) noexcept;
 
+    template <typename Integer>
+    friend constexpr auto operator<=(decimal32 lhs, Integer rhs) noexcept -> std::enable_if_t<detail::is_integral_v<Integer>, bool>;
+
+    template <typename Integer>
+    friend constexpr auto operator<=(Integer lhs, decimal32 rhs) noexcept -> std::enable_if_t<detail::is_integral_v<Integer>, bool>;
+
     // Greater
     friend constexpr bool operator>(decimal32 lhs, decimal32 rhs) noexcept;
 
@@ -1244,6 +1250,18 @@ constexpr auto operator<(Integer lhs, decimal32 rhs) noexcept -> std::enable_if_
 }
 
 constexpr bool operator<=(decimal32 lhs, decimal32 rhs) noexcept
+{
+    return !(rhs < lhs);
+}
+
+template <typename Integer>
+constexpr auto operator<=(decimal32 lhs, Integer rhs) noexcept -> std::enable_if_t<detail::is_integral_v<Integer>, bool>
+{
+    return !(rhs < lhs);
+}
+
+template <typename Integer>
+constexpr auto operator<=(Integer lhs, decimal32 rhs) noexcept -> std::enable_if_t<detail::is_integral_v<Integer>, bool>
 {
     return !(rhs < lhs);
 }
