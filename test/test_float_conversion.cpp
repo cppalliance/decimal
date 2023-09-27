c6c58d9e155fea827c647fbed41d517546e7ec69
Matt Borland <matt@mattborland.com>
2023-09-27 10:13:11 +0200

    Force run-time evaluation

--- test/test_float_conversion.cpp
+++ test/test_float_conversion.cpp
@@ -5,59 +5,69 @@
 #include <boost/decimal.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <iostream>
+#include <random>
 #include <cmath>
 
 void test_compute_float32()
 {
     using boost::decimal::detail::fast_float::compute_float32;
 
+    std::mt19937_64 gen;
+    std::uniform_real_distribution<float> dist (1, 1);
+
     bool success;
 
     // Trivial verifcation
-    BOOST_TEST_EQ(compute_float32(1, 1, false, success), 1e1F);
-    BOOST_TEST_EQ(compute_float32(0, 1, true, success), -1e0F);
-    BOOST_TEST_EQ(compute_float32(38, 1, false, success), 1e38F);
+    BOOST_TEST_EQ(compute_float32(1 * dist(gen), 1, false, success), 1e1F);
+    BOOST_TEST_EQ(compute_float32(0 * dist(gen), 1, true, success), -1e0F);
+    BOOST_TEST_EQ(compute_float32(38 * dist(gen), 1, false, success), 1e38F);
 
     // out of range
-    BOOST_TEST_EQ(compute_float32(310, 5, false, success), HUGE_VALF);
-    BOOST_TEST_EQ(compute_float32(310, 5, true, success), -HUGE_VALF);
-    BOOST_TEST_EQ(compute_float32(1000, 5, false, success), HUGE_VALF);
-    BOOST_TEST_EQ(compute_float32(1000, 5, true, success), -HUGE_VALF);
-    BOOST_TEST_EQ(compute_float32(-325, 5, false, success), 0);
+    BOOST_TEST_EQ(compute_float32(310 * dist(gen), 5, false, success), HUGE_VALF);
+    BOOST_TEST_EQ(compute_float32(310 * dist(gen), 5, true, success), -HUGE_VALF);
+    BOOST_TEST_EQ(compute_float32(1000 * dist(gen), 5, false, success), HUGE_VALF);
+    BOOST_TEST_EQ(compute_float32(1000 * dist(gen), 5, true, success), -HUGE_VALF);
+    BOOST_TEST_EQ(compute_float32(-325 * dist(gen), 5, false, success), 0);
 
     // Composite
-    BOOST_TEST_EQ(compute_float32(10, 123456789, false, success), 123456789e10F);
-    BOOST_TEST_EQ(compute_float32(20, UINT64_C(444444444), false, success), 444444444e20F);
+    BOOST_TEST_EQ(compute_float32(10 * dist(gen), 123456789, false, success), 123456789e10F);
+    BOOST_TEST_EQ(compute_float32(20 * dist(gen), UINT64_C(444444444), false, success), 444444444e20F);
 }
 
 void test_compute_float64()
 {
     using boost::decimal::detail::fast_float::compute_float64;
 
+    std::mt19937_64 gen;
+    std::uniform_real_distribution<double> dist (1, 1);
+
     bool success;
 
     // Trivial verifcation
-    BOOST_TEST_EQ(compute_float64(1, 1, false, success), 1e1);
-    BOOST_TEST_EQ(compute_float64(0, 1, true, success), -1e0);
-    BOOST_TEST_EQ(compute_float64(308, 1, false, success), 1e308);
+    BOOST_TEST_EQ(compute_float64(1, dist(gen), false, success), 1e1);
+    BOOST_TEST_EQ(compute_float64(0, dist(gen), true, success), -1e0);
+    BOOST_TEST_EQ(compute_float64(308, dist(gen), false, success), 1e308);
 
     // out of range
-    BOOST_TEST_EQ(compute_float64(310, 5, false, success), HUGE_VALF);
-    BOOST_TEST_EQ(compute_float64(310, 5, true, success), -HUGE_VALF);
-    BOOST_TEST_EQ(compute_float64(1000, 5, false, success), HUGE_VALF);
-    BOOST_TEST_EQ(compute_float64(1000, 5, true, success), -HUGE_VALF);
+    BOOST_TEST_EQ(compute_float64(310, 5, false, success), HUGE_VALF * dist(gen));
+    BOOST_TEST_EQ(compute_float64(310, 5, true, success), -HUGE_VALF * dist(gen));
+    BOOST_TEST_EQ(compute_float64(1000, 5, false, success), HUGE_VALF * dist(gen));
+    BOOST_TEST_EQ(compute_float64(1000, 5, true, success), -HUGE_VALF * dist(gen));
     BOOST_TEST_EQ(compute_float64(-325, 5, false, success), 0);
 
     // Composite
-    BOOST_TEST_EQ(compute_float64(10, 123456789, false, success), 123456789e10);
-    BOOST_TEST_EQ(compute_float64(100, UINT64_C(4444444444444444444), false, success), 4444444444444444444e100);
-    BOOST_TEST_EQ(compute_float64(100, std::numeric_limits<std::uint64_t>::max(), false, success), 18446744073709551615e100);
-    BOOST_TEST_EQ(compute_float64(100, UINT64_C(10000000000000000000), false, success), 10000000000000000000e100);
+    BOOST_TEST_EQ(compute_float64(10 * dist(gen), 123456789, false, success), 123456789e10);
+    BOOST_TEST_EQ(compute_float64(100 * dist(gen), UINT64_C(4444444444444444444), false, success), 4444444444444444444e100);
+    BOOST_TEST_EQ(compute_float64(100 * dist(gen), std::numeric_limits<std::uint64_t>::max(), false, success), 18446744073709551615e100);
+    BOOST_TEST_EQ(compute_float64(100 * dist(gen), UINT64_C(10000000000000000000), false, success), 10000000000000000000e100);
 }
 
 template <typename T>
 void test_generic_binary_to_decimal()
 {
+    std::mt19937_64 gen;
+    std::uniform_real_distribution<T> dist (1, 2);
+
     using namespace boost::decimal::detail::ryu;
 
     std::uint64_t* result;
@@ -68,11 +78,11 @@ void test_generic_binary_to_decimal()
     BOOST_TEST_EQ(result[2], 4575641699882439235);
     free(result);
 
-    BOOST_TEST(floating_point_to_fd128(T(0)).mantissa == 0);
-    BOOST_TEST(floating_point_to_fd128(std::numeric_limits<T>::infinity()).exponent == fd128_exceptional_exponent);
-    BOOST_TEST(floating_point_to_fd128(std::numeric_limits<T>::quiet_NaN()).exponent == fd128_exceptional_exponent);
-    BOOST_TEST(floating_point_to_fd128(-std::numeric_limits<T>::infinity()).exponent == fd128_exceptional_exponent);
-    BOOST_TEST(floating_point_to_fd128(-std::numeric_limits<T>::quiet_NaN()).exponent == fd128_exceptional_exponent);
+    BOOST_TEST(floating_point_to_fd128(T(0) * dist(gen)).mantissa == 0);
+    BOOST_TEST(floating_point_to_fd128(std::numeric_limits<T>::infinity() * dist(gen)).exponent == fd128_exceptional_exponent);
+    BOOST_TEST(floating_point_to_fd128(std::numeric_limits<T>::quiet_NaN() * dist(gen)).exponent == fd128_exceptional_exponent);
+    BOOST_TEST(floating_point_to_fd128(-std::numeric_limits<T>::infinity() * dist(gen)).exponent == fd128_exceptional_exponent);
+    BOOST_TEST(floating_point_to_fd128(-std::numeric_limits<T>::quiet_NaN() * dist(gen)).exponent == fd128_exceptional_exponent);
 }
 
 int main()
