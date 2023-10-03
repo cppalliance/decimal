899c23810bbe35f59d899a69392c2ea3769cdb1c
Matt Borland <matt@mattborland.com>
2023-10-03 09:23:15 +0200

    Add isinf

--- include/boost/decimal/decimal64.hpp
+++ include/boost/decimal/decimal64.hpp
@@ -148,6 +148,7 @@ public:
     constexpr decimal64(T1 coeff, T2 exp, bool sign = false) noexcept;
     friend constexpr auto signbit     BOOST_DECIMAL_PREVENT_MACRO_SUBSTITUTION (decimal64 rhs) noexcept -> bool;
     friend constexpr auto isnan       BOOST_DECIMAL_PREVENT_MACRO_SUBSTITUTION (decimal64 rhs) noexcept -> bool;
+    friend constexpr auto isinf       BOOST_DECIMAL_PREVENT_MACRO_SUBSTITUTION (decimal64 rhs) noexcept -> bool;
 };
 
 // 3.2.5 initialization from coefficient and exponent:
@@ -382,6 +383,11 @@ constexpr auto signbit BOOST_DECIMAL_PREVENT_MACRO_SUBSTITUTION (decimal64 rhs) 
 constexpr auto isnan BOOST_DECIMAL_PREVENT_MACRO_SUBSTITUTION (decimal64 rhs) noexcept -> bool
 {
     return (rhs.bits_.combination_field & detail::d64_comb_nan_mask) == detail::d64_comb_nan_mask;
+}
+
+constexpr auto isinf BOOST_DECIMAL_PREVENT_MACRO_SUBSTITUTION (decimal64 rhs) noexcept -> bool
+{
+    return ((rhs.bits_.combination_field & detail::d64_comb_inf_mask) == detail::d64_comb_inf_mask) && (!isnan(rhs));
 }
 
 } //namespace decimal
