a1ce3208b65249f8310f36e40b4320a52d6440ad
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-10-11 14:49:58 +0200

    Improve and test sqrt

---
+++ test/test_sqrt.cpp
@@ -0,0 +1,252 @@
+// Copyright 2023 Matt Borland
+// Copyright 2023 Christopher Kormanyos
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#if defined(__clang__)
+  #if defined __has_feature
+  #if __has_feature(thread_sanitizer)
+  #define BOOST_DECIMAL_REDUCE_TEST_DEPTH
+  #endif
+  #endif
+#elif defined(__GNUC__)
+  #if defined(__SANITIZE_THREAD__)
+  #define BOOST_DECIMAL_REDUCE_TEST_DEPTH
+  #endif
+#elif defined(_MSC_VER)
+  #if defined(_DEBUG)
+  #define BOOST_DECIMAL_REDUCE_TEST_DEPTH
+  #endif
+#endif
+
+#include <chrono>
+#include <limits>
+#include <random>
+
+#include <boost/decimal.hpp>
+#include <boost/core/lightweight_test.hpp>
+
+auto my_zero() -> boost::decimal::decimal32&;
+auto my_one () -> boost::decimal::decimal32&;
+
+namespace local
+{
+  template<typename IntegralTimePointType,
+           typename ClockType = std::chrono::high_resolution_clock>
+  auto time_point() noexcept -> IntegralTimePointType
+  {
+    using local_integral_time_point_type = IntegralTimePointType;
+    using local_clock_type               = ClockType;
+
+    const auto current_now =
+      static_cast<std::uintmax_t>
+      (
+        std::chrono::duration_cast<std::chrono::nanoseconds>
+        (
+          local_clock_type::now().time_since_epoch()
+        ).count()
+      );
+
+    return static_cast<local_integral_time_point_type>(current_now);
+  }
+
+  template<typename NumericType>
+  auto is_close_fraction(const NumericType& a,
+                         const NumericType& b,
+                         const NumericType& tol) noexcept -> bool
+  {
+    using std::fabs;
+
+    auto result_is_ok = bool { };
+
+    if(b == static_cast<NumericType>(0))
+    {
+      result_is_ok = (fabs(a - b) < tol);
+    }
+    else
+    {
+      const auto delta = fabs(1 - fabs(a / b));
+
+      result_is_ok = (delta < tol);
+    }
+
+    return result_is_ok;
+  }
+
+  auto test_sqrt(const std::int32_t tol_factor, const long double range_lo, const long double range_hi) -> bool
+  {
+    using decimal_type = boost::decimal::decimal32;
+
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+
+    gen.seed(time_point<typename std::mt19937_64::result_type>());
+
+    auto dis =
+      std::uniform_real_distribution<float>
+      {
+        static_cast<float>(range_lo),
+        static_cast<float>(range_hi)
+      };
+
+    auto result_is_ok = true;
+
+    auto trials = static_cast<std::uint32_t>(UINT8_C(0));
+
+    #if !defined(BOOST_DECIMAL_REDUCE_TEST_DEPTH)
+    constexpr auto count = static_cast<std::uint32_t>(UINT32_C(0x2000));
+    #else
+    constexpr auto count = static_cast<std::uint32_t>(UINT32_C(0x200));
+    #endif
+
+    for( ; trials < count; ++trials)
+    {
+      const auto x_flt = dis(gen);
+      const auto x_dec = static_cast<decimal_type>(x_flt);
+
+      using std::sqrt;
+
+      const auto val_flt = sqrt(x_flt);
+      const auto val_dec = sqrt(x_dec);
+
+      const auto result_val_is_ok = is_close_fraction(val_flt, static_cast<float>(val_dec), std::numeric_limits<float>::epsilon() * tol_factor);
+
+      result_is_ok = (result_val_is_ok && result_is_ok);
+
+      if(!result_val_is_ok)
+      {
+        std::cout << "x_flt  : " <<                    x_flt   << std::endl;
+        std::cout << "val_flt: " << std::scientific << val_flt << std::endl;
+        std::cout << "val_dec: " << std::scientific << val_dec << std::endl;
+
+        break;
+      }
+    }
+
+    BOOST_TEST(result_is_ok);
+
+    return result_is_ok;
+  }
+
+  auto test_sqrt_edge() -> bool
+  {
+    using decimal_type = boost::decimal::decimal32;
+
+    std::mt19937_64 gen;
+
+    std::uniform_real_distribution<float> dist(1.01F, 1.04F);
+
+    auto result_is_ok = true;
+
+    for(auto i = static_cast<unsigned>(UINT8_C(0)); i < static_cast<unsigned>(UINT8_C(10)); ++i)
+    {
+      static_cast<void>(i);
+
+      const auto val_nan_pos = sqrt(std::numeric_limits<decimal_type>::quiet_NaN() * static_cast<decimal_type>(dist(gen)));
+
+      const auto result_val_nan_pos_is_ok = isnan(val_nan_pos) && (!signbit(val_nan_pos));
+
+      BOOST_TEST(result_val_nan_pos_is_ok);
+
+      result_is_ok = (result_val_nan_pos_is_ok && result_is_ok);
+    }
+
+    for(auto i = static_cast<unsigned>(UINT8_C(0)); i < static_cast<unsigned>(UINT8_C(10)); ++i)
+    {
+      static_cast<void>(i);
+
+      const auto val_nan_neg = sqrt(-std::numeric_limits<decimal_type>::quiet_NaN() * static_cast<decimal_type>(dist(gen)));
+
+      const auto result_val_nan_neg_is_ok = isnan(val_nan_neg) && (signbit(val_nan_neg));
+
+      BOOST_TEST(result_val_nan_neg_is_ok);
+
+      result_is_ok = (result_val_nan_neg_is_ok && result_is_ok);
+    }
+
+    for(auto i = static_cast<unsigned>(UINT8_C(0)); i < static_cast<unsigned>(UINT8_C(10)); ++i)
+    {
+      static_cast<void>(i);
+
+      const auto val_inf_pos = sqrt(std::numeric_limits<decimal_type>::infinity() * static_cast<decimal_type>(dist(gen)));
+
+      const auto result_val_inf_pos_is_ok = (isinf(val_inf_pos) && (!signbit(val_inf_pos)));
+
+      BOOST_TEST(result_val_inf_pos_is_ok);
+
+      result_is_ok = (result_val_inf_pos_is_ok && result_is_ok);
+    }
+
+    for(auto i = static_cast<unsigned>(UINT8_C(0)); i < static_cast<unsigned>(UINT8_C(10)); ++i)
+    {
+      static_cast<void>(i);
+
+      const auto val_one = sqrt(::my_one());
+
+      const auto result_val_one_is_ok = (val_one == ::my_one());
+
+      BOOST_TEST(result_val_one_is_ok);
+
+      result_is_ok = (result_val_one_is_ok && result_is_ok);
+    }
+
+    for(auto i = static_cast<unsigned>(UINT8_C(0)); i < static_cast<unsigned>(UINT8_C(10)); ++i)
+    {
+      static_cast<void>(i);
+
+      const auto val_zero_pos = sqrt(::my_zero());
+
+      const auto result_val_zero_pos_is_ok = ((val_zero_pos == ::my_zero()) && (!signbit(val_zero_pos)));
+
+      BOOST_TEST(result_val_zero_pos_is_ok);
+
+      result_is_ok = (result_val_zero_pos_is_ok && result_is_ok);
+    }
+
+    for(auto i = static_cast<unsigned>(UINT8_C(0)); i < static_cast<unsigned>(UINT8_C(10)); ++i)
+    {
+      static_cast<void>(i);
+
+      const auto val_zero_neg = sqrt(-::my_zero());
+
+      const auto result_val_zero_neg_is_ok = ((val_zero_neg == -::my_zero()) && signbit(val_zero_neg));
+
+      BOOST_TEST(result_val_zero_neg_is_ok);
+
+      result_is_ok = (result_val_zero_neg_is_ok && result_is_ok);
+    }
+
+    return result_is_ok;
+  }
+
+} // namespace local
+
+auto main() -> int
+{
+  auto result_is_ok = true;
+
+  const auto result_small_is_ok  = local::test_sqrt(static_cast<std::int32_t>(INT32_C(16)), 1.0E-26L, 1.0E-01L);
+  const auto result_medium_is_ok = local::test_sqrt(static_cast<std::int32_t>(INT32_C(16)), 0.9E-01L, 1.1E+01L);
+  const auto result_large_is_ok  = local::test_sqrt(static_cast<std::int32_t>(INT32_C(16)), 1.0E+01L, 1.0E+26L);
+
+  BOOST_TEST(result_small_is_ok);
+  BOOST_TEST(result_medium_is_ok);
+  BOOST_TEST(result_large_is_ok);
+
+  const auto result_edge_is_ok = local::test_sqrt_edge();
+
+  const auto result_ranges_is_ok = (result_small_is_ok && result_medium_is_ok && result_large_is_ok);
+
+  result_is_ok = (result_ranges_is_ok && result_is_ok);
+
+  BOOST_TEST(result_edge_is_ok);
+
+  result_is_ok = (result_edge_is_ok && result_is_ok);
+
+  result_is_ok = ((boost::report_errors() == 0) && result_is_ok);
+
+  return (result_is_ok ? 0 : -1);
+}
+
+auto my_zero() -> boost::decimal::decimal32& { static boost::decimal::decimal32 val_zero { 0, 0 }; return val_zero; }
+auto my_one () -> boost::decimal::decimal32& { static boost::decimal::decimal32 val_one  { 1, 0 }; return val_one; }
