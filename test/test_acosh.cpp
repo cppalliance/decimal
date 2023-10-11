ce52744cd9a9a92efcf2bda4c69f3770dba00999
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-10-11 10:57:00 +0200

    Finish acosh and repair sqrt

--- test/test_acosh.cpp
+++ test/test_acosh.cpp
@@ -225,17 +225,19 @@ auto main() -> int
 {
   auto result_is_ok = true;
 
-  const auto result_small_is_ok  = local::test_acosh(128, 1.45L, 1.59L);
-  const auto result_medium_is_ok = local::test_acosh(384, 1.59L, 10.1L);
+  const auto result_tiny_is_ok  = local::test_acosh(2048, 1.001L, 1.1L);
+  const auto result_small_is_ok  = local::test_acosh(64, 1.1L, 1.59L);
+  const auto result_medium_is_ok = local::test_acosh(48, 1.59L, 10.1L);
   const auto result_large_is_ok  = local::test_acosh(48, 1.0E+01L, 1.0E+26L);
 
+  BOOST_TEST(result_tiny_is_ok);
   BOOST_TEST(result_small_is_ok);
   BOOST_TEST(result_medium_is_ok);
   BOOST_TEST(result_large_is_ok);
 
   const auto result_edge_is_ok = local::test_acosh_edge();
 
-  const auto result_ranges_is_ok = (result_small_is_ok && result_medium_is_ok && result_large_is_ok);
+  const auto result_ranges_is_ok = (result_tiny_is_ok && result_small_is_ok && result_medium_is_ok && result_large_is_ok);
 
   result_is_ok = (result_ranges_is_ok && result_is_ok);
 
