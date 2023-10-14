8afed62dc932a477082c9cc968f94b63741520cb
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-10-14 11:26:01 +0200

    Update and test exp 64-bit and Pade approx

--- test/test_exp.cpp
+++ test/test_exp.cpp
@@ -3,6 +3,22 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#if defined(__clang__)
+  #if defined __has_feature
+  #if __has_feature(thread_sanitizer)
+  #define BOOST_DECIMAL_REDUCE_TEST_DEPTH
+  #endif
+  #endif
+#elif defined(__GNUC__)
+  #if defined(__SANITIZE_THREAD__)
+  #define BOOST_DECIMAL_REDUCE_TEST_DEPTH
+  #endif
+#elif defined(_MSC_VER)
+  #if defined(_DEBUG)
+  #define BOOST_DECIMAL_REDUCE_TEST_DEPTH
+  #endif
+#endif
+
 #include <chrono>
 #include <limits>
 #include <random>
@@ -10,8 +26,8 @@
 #include <boost/decimal.hpp>
 #include <boost/core/lightweight_test.hpp>
 
-auto my_zero() -> boost::decimal::decimal32&;
-auto my_one () -> boost::decimal::decimal32&;
+template<typename DecimalType> auto my_zero() -> DecimalType& { using decimal_type = DecimalType; static decimal_type val_zero { 0, 0 }; return val_zero; }
+template<typename DecimalType> auto my_one () -> DecimalType& { using decimal_type = DecimalType; static decimal_type val_one  { 1, 0 }; return val_one; }
 
 namespace local
 {
@@ -57,9 +73,11 @@ namespace local
     return result_is_ok;
   }
 
+  template<typename DecimalType, typename FloatType>
   auto test_exp(const int tol_factor, const bool negate, const long double range_lo, const long double range_hi) -> bool
   {
-    using decimal_type = boost::decimal::decimal32;
+    using decimal_type = DecimalType;
+    using float_type   = FloatType;
 
     std::random_device rd;
     std::mt19937_64 gen(rd());
@@ -67,10 +85,10 @@ namespace local
     gen.seed(time_point<typename std::mt19937_64::result_type>());
 
     auto dis =
-      std::uniform_real_distribution<float>
+      std::uniform_real_distribution<float_type>
       {
-        static_cast<float>(range_lo),
-        static_cast<float>(range_hi)
+        static_cast<float_type>(range_lo),
+        static_cast<float_type>(range_hi)
       };
 
     auto result_is_ok = true;
@@ -78,9 +96,9 @@ namespace local
     auto trials = static_cast<std::uint32_t>(UINT8_C(0));
 
     #if !defined(BOOST_DECIMAL_REDUCE_TEST_DEPTH)
-    constexpr auto count = static_cast<std::uint32_t>(UINT32_C(0x2000));
+    constexpr auto count = (sizeof(decimal_type) == static_cast<std::size_t>(UINT8_C(4))) ? static_cast<std::uint32_t>(UINT32_C(0x2000)) : static_cast<std::uint32_t>(UINT32_C(0x400));
     #else
-    constexpr auto count = static_cast<std::uint32_t>(UINT32_C(0x200));
+    constexpr auto count = (sizeof(decimal_type) == static_cast<std::size_t>(UINT8_C(4))) ? static_cast<std::uint32_t>(UINT32_C(0x200)) : static_cast<std::uint32_t>(UINT32_C(0x40));
     #endif
 
     for( ; trials < count; ++trials)
@@ -95,15 +113,15 @@ namespace local
       const auto val_flt = exp(x_flt);
       const auto val_dec = exp(x_dec);
 
-      const auto result_val_is_ok = is_close_fraction(val_flt, static_cast<float>(val_dec), std::numeric_limits<float>::epsilon() * tol_factor);
+      const auto result_val_is_ok = is_close_fraction(val_flt, static_cast<float_type>(val_dec), static_cast<float_type>(std::numeric_limits<decimal_type>::epsilon()) * tol_factor);
 
       result_is_ok = (result_val_is_ok && result_is_ok);
 
       if(!result_val_is_ok)
       {
-        std::cout << "x_flt  : " <<                    x_flt   << std::endl;
-        std::cout << "val_flt: " << std::scientific << val_flt << std::endl;
-        std::cout << "val_dec: " << std::scientific << val_dec << std::endl;
+        std::cout << "x_flt  : " << std::scientific << std::setprecision(std::numeric_limits<float_type>::digits10) << x_flt   << std::endl;
+        std::cout << "val_flt: " << std::scientific << std::setprecision(std::numeric_limits<float_type>::digits10) << val_flt << std::endl;
+        std::cout << "val_dec: " << std::scientific << std::setprecision(std::numeric_limits<float_type>::digits10) << val_dec << std::endl;
 
         break;
       }
@@ -114,13 +132,20 @@ namespace local
     return result_is_ok;
   }
 
+  template<typename DecimalType, typename FloatType>
   auto test_exp_edge() -> bool
   {
-    using decimal_type = boost::decimal::decimal32;
+    using decimal_type = DecimalType;
+    using float_type   = FloatType;
 
     std::mt19937_64 gen;
 
-    std::uniform_real_distribution<float> dist(1.01F, 1.04F);
+    std::uniform_real_distribution<float_type>
+      dist
+      (
+        static_cast<float_type>(1.01L),
+        static_cast<float_type>(1.04L)
+      );
 
     auto result_is_ok = true;
 
@@ -156,7 +181,7 @@ namespace local
 
       const auto val_inf_neg = exp(-std::numeric_limits<decimal_type>::infinity() * static_cast<decimal_type>(dist(gen)));
 
-      const auto result_val_inf_neg_is_ok = (val_inf_neg == ::my_zero());
+      const auto result_val_inf_neg_is_ok = (val_inf_neg == ::my_zero<decimal_type>());
 
       BOOST_TEST(result_val_inf_neg_is_ok);
 
@@ -167,9 +192,9 @@ namespace local
     {
       static_cast<void>(i);
 
-      const auto val_zero_pos = exp(::my_zero());
+      const auto val_zero_pos = exp(::my_zero<decimal_type>());
 
-      const auto result_val_zero_pos_is_ok = (val_zero_pos == ::my_one());
+      const auto result_val_zero_pos_is_ok = (val_zero_pos == ::my_one<decimal_type>());
 
       BOOST_TEST(result_val_zero_pos_is_ok);
 
@@ -180,9 +205,9 @@ namespace local
     {
       static_cast<void>(i);
 
-      const auto val_zero_neg = exp(-::my_zero());
+      const auto val_zero_neg = exp(-::my_zero<decimal_type>());
 
-      const auto result_val_zero_neg_is_ok = (val_zero_neg == ::my_one());
+      const auto result_val_zero_neg_is_ok = (val_zero_neg == ::my_one<decimal_type>());
 
       BOOST_TEST(result_val_zero_neg_is_ok);
 
@@ -191,50 +216,63 @@ namespace local
 
     return result_is_ok;
   }
-
 } // namespace local
+
+namespace
+{
+  template<typename DecimalType, typename FloatType>
+  auto test_all() -> bool
+  {
+    using decimal_type = DecimalType;
+    using float_type   = FloatType;
+
+    auto result_is_ok = true;
+
+    const auto result_pos_is_ok = local::test_exp<decimal_type, float_type>(128, false, 0.03125L, 32.0L);
+    const auto result_neg_is_ok = local::test_exp<decimal_type, float_type>(128, true,  0.03125L, 32.0L);
+
+    const auto result_pos_narrow_is_ok = local::test_exp<decimal_type, float_type>(64, false, 0.25L, 4.0L);
+    const auto result_neg_narrow_is_ok = local::test_exp<decimal_type, float_type>(64, true,  0.25L, 4.0L);
+
+    const auto result_edge_is_ok = local::test_exp_edge<decimal_type, float_type>();
+
+    BOOST_TEST(result_pos_is_ok);
+    BOOST_TEST(result_neg_is_ok);
+
+    BOOST_TEST(result_pos_narrow_is_ok);
+    BOOST_TEST(result_neg_narrow_is_ok);
+
+    BOOST_TEST(result_edge_is_ok);
+
+    result_is_ok = (result_pos_is_ok && result_is_ok);
+    result_is_ok = (result_neg_is_ok && result_is_ok);
+
+    result_is_ok = (result_pos_narrow_is_ok && result_is_ok);
+    result_is_ok = (result_neg_narrow_is_ok && result_is_ok);
+
+    result_is_ok = (result_edge_is_ok && result_is_ok);
+
+    return result_is_ok;
+  }
+} // namespace
 
 auto main() -> int
 {
   auto result_is_ok = true;
 
-  const auto result_pos_is_ok = local::test_exp(96, false, 0.03125L, 32.0L);
-  const auto result_neg_is_ok = local::test_exp(96, true,  0.03125L, 32.0L);
+  {
+    const auto result_decimal32_is_ok = test_all<boost::decimal::decimal32, float>();
 
-  const auto result_pos_narrow_is_ok = local::test_exp(16, false, 0.25L, 4.0L);
-  const auto result_neg_narrow_is_ok = local::test_exp(24, true,  0.25L, 4.0L);
+    result_is_ok = result_is_ok && result_decimal32_is_ok;
+  }
 
-  const auto result_pos_wide_is_ok = local::test_exp(112, false, 0.0125L, 80.0L);
-  const auto result_neg_wide_is_ok = local::test_exp(112, true,  0.0125L, 80.0L);
+  {
+    const auto result_decimal64_is_ok = test_all<boost::decimal::decimal64, double>();
 
-  const auto result_edge_is_ok = local::test_exp_edge();
-
-  BOOST_TEST(result_pos_is_ok);
-  BOOST_TEST(result_neg_is_ok);
-
-  BOOST_TEST(result_pos_narrow_is_ok);
-  BOOST_TEST(result_neg_narrow_is_ok);
-
-  BOOST_TEST(result_pos_wide_is_ok);
-  BOOST_TEST(result_neg_wide_is_ok);
-
-  BOOST_TEST(result_edge_is_ok);
-
-  result_is_ok = (result_pos_is_ok  && result_is_ok);
-  result_is_ok = (result_neg_is_ok  && result_is_ok);
-
-  result_is_ok = (result_pos_narrow_is_ok  && result_is_ok);
-  result_is_ok = (result_neg_narrow_is_ok  && result_is_ok);
-
-  result_is_ok = (result_pos_wide_is_ok  && result_is_ok);
-  result_is_ok = (result_neg_wide_is_ok  && result_is_ok);
-
-  result_is_ok = (result_edge_is_ok && result_is_ok);
+    result_is_ok = result_is_ok && result_decimal64_is_ok;
+  }
 
   result_is_ok = ((boost::report_errors() == 0) && result_is_ok);
 
   return (result_is_ok ? 0 : -1);
 }
-
-auto my_zero() -> boost::decimal::decimal32& { static boost::decimal::decimal32 val_zero { 0, 0 }; return val_zero; }
-auto my_one () -> boost::decimal::decimal32& { static boost::decimal::decimal32 val_one  { 1, 0 }; return val_one; }
