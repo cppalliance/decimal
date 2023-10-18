020a954ccdd69922d578924b1fa8130b7ae8840b
Matt Borland <matt@mattborland.com>
2023-10-18 11:06:16 +0200

    Add mixed multiplication testing

--- test/random_mixed_decimal_math.cpp
+++ test/random_mixed_decimal_math.cpp
@@ -79,7 +79,7 @@ void random_mixed_subtraction(T lower, T upper)
                       << "\nVal 2: " << val2
                       << "\nDec 2: " << dec2
                       << "\nDec res: " << res
-                      << "\nInt res: " << val1 + val2 << std::endl;
+                      << "\nInt res: " << val1 - val2 << std::endl;
         }
     }
 
@@ -87,6 +87,39 @@ void random_mixed_subtraction(T lower, T upper)
     BOOST_TEST(isinf(Decimal2(dist(rng)) - std::numeric_limits<Decimal1>::infinity()));
     BOOST_TEST(isnan(std::numeric_limits<Decimal1>::quiet_NaN() - Decimal2(dist(rng))));
     BOOST_TEST(isnan(Decimal2(dist(rng)) - std::numeric_limits<Decimal1>::quiet_NaN()));
+}
+
+template <typename Decimal1, typename Decimal2, typename T>
+void random_mixed_multiplication(T lower, T upper)
+{
+    std::uniform_int_distribution<T> dist(lower, upper);
+
+    for (std::size_t i {}; i < N; ++i)
+    {
+        const T val1 {dist(rng)};
+        const T val2 {dist(rng)};
+
+        const Decimal1 dec1 {val1};
+        const Decimal2 dec2 {val2};
+
+        const auto res = dec1 * dec2;
+        const auto res_int = static_cast<T>(res);
+
+        if (!BOOST_TEST_EQ(res_int, val1 * val2))
+        {
+            std::cerr << "Val 1: " << val1
+                      << "\nDec 1: " << dec1
+                      << "\nVal 2: " << val2
+                      << "\nDec 2: " << dec2
+                      << "\nDec res: " << res
+                      << "\nInt res: " << val1 * val2 << std::endl;
+        }
+    }
+
+    BOOST_TEST(isinf(std::numeric_limits<Decimal1>::infinity() * Decimal2(dist(rng))));
+    BOOST_TEST(isinf(Decimal2(dist(rng)) * std::numeric_limits<Decimal1>::infinity()));
+    BOOST_TEST(isnan(std::numeric_limits<Decimal1>::quiet_NaN() * Decimal2(dist(rng))));
+    BOOST_TEST(isnan(Decimal2(dist(rng)) * std::numeric_limits<Decimal1>::quiet_NaN()));
 }
 
 int main()
@@ -120,6 +153,21 @@ int main()
     random_mixed_subtraction<decimal32, decimal64>(-5'000'000LL, 5'000'000LL);
     random_mixed_subtraction<decimal64, decimal32>(-5'000'000, 5'000'000);
     random_mixed_subtraction<decimal64, decimal32>(-5'000'000LL, 5'000'000LL);
+
+    random_mixed_multiplication<decimal32, decimal64>(0, 5'000'000);
+    random_mixed_multiplication<decimal32, decimal64>(0LL, 5'000'000LL);
+    random_mixed_multiplication<decimal64, decimal32>(0, 5'000'000);
+    random_mixed_multiplication<decimal64, decimal32>(0LL, 5'000'000LL);
+
+    random_mixed_multiplication<decimal32, decimal64>(-5'000'000, 0);
+    random_mixed_multiplication<decimal32, decimal64>(-5'000'000LL, 0LL);
+    random_mixed_multiplication<decimal64, decimal32>(-5'000'000, 0);
+    random_mixed_multiplication<decimal64, decimal32>(-5'000'000LL, 0LL);
+
+    random_mixed_multiplication<decimal32, decimal64>(-5'000'000, 5'000'000);
+    random_mixed_multiplication<decimal32, decimal64>(-5'000'000LL, 5'000'000LL);
+    random_mixed_multiplication<decimal64, decimal32>(-5'000'000, 5'000'000);
+    random_mixed_multiplication<decimal64, decimal32>(-5'000'000LL, 5'000'000LL);
 
     return boost::report_errors();
 }
