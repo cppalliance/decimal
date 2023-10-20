5f1ea4bbee730aff077094fcd3b4db55797324bf
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-10-20 07:36:44 +0200

    Repair prototypes of pow

--- include/boost/decimal/detail/cmath/pow.hpp
+++ include/boost/decimal/detail/cmath/pow.hpp
@@ -14,8 +14,56 @@
 
 namespace boost { namespace decimal {
 
-template<typename T, std::enable_if_t<detail::is_decimal_floating_point_v<T>, bool> = true>
-constexpr auto pow(T x, T a) noexcept -> T
+template<typename T, typename IntegralType>
+constexpr auto pow(T b, IntegralType p) noexcept -> std::enable_if_t<detail::is_decimal_floating_point_v<T> && std::is_integral_v<IntegralType>, T> // NOLINT(misc-no-recursion)
+{
+    // Calculate (b ^ p).
+
+    using local_numeric_type = T;
+
+    local_numeric_type result;
+
+    if     (p <  INT64_C(0)) { result = local_numeric_type(1) / pow(b, -p); }
+    else if(p == INT64_C(0)) { result = local_numeric_type(static_cast<unsigned>(UINT8_C(1))); }
+    else if(p == INT64_C(1)) { result = b; }
+    else if(p == INT64_C(2)) { result = b; result *= b; }
+    else if(p == INT64_C(3)) { result = b; result *= b; result *= b; }
+    else if(p == INT64_C(4)) { result = b; result *= b; result *= result; }
+    else
+    {
+        result = local_numeric_type(static_cast<unsigned>(UINT8_C(1)));
+
+        local_numeric_type y(b);
+
+        auto p_local = static_cast<std::uint64_t>(p);
+
+        // Use the so-called ladder method for the power calculation.
+        for(;;)
+        {
+            const auto do_power_multiply =
+              (static_cast<std::uint_fast8_t>(p_local & static_cast<unsigned>(UINT8_C(1))) != static_cast<std::uint_fast8_t>(UINT8_C(0)));
+
+            if(do_power_multiply)
+            {
+              result *= y;
+            }
+
+            p_local >>= static_cast<unsigned>(UINT8_C(1));
+
+            if(p_local == static_cast<std::uint64_t>(UINT8_C(0)))
+            {
+                break;
+            }
+
+            y *= y;
+        }
+    }
+
+    return result;
+}
+
+template<typename T>
+constexpr auto pow(T x, T a) noexcept -> std::enable_if_t<detail::is_decimal_floating_point_v<T>, T>
 {
     const auto fpc_x = fpclassify(x);
     const auto fpc_a = fpclassify(a);
@@ -92,54 +140,6 @@ constexpr auto pow(T x, T a) noexcept -> T
             const auto a_log_x = a * log(x);
 
             result = exp(a_log_x);
-        }
-    }
-
-    return result;
-}
-
-template<typename T, std::enable_if_t<detail::is_decimal_floating_point_v<T>, bool> = true>
-constexpr auto pow(T b, int p) noexcept -> T
-{
-    // Calculate (b ^ p).
-
-    using local_numeric_type = T;
-
-    local_numeric_type result;
-
-    if     (p <  INT64_C(0)) { result = local_numeric_type(1) / pow(b, -p); }
-    else if(p == INT64_C(0)) { result = local_numeric_type(static_cast<unsigned>(UINT8_C(1))); }
-    else if(p == INT64_C(1)) { result = b; }
-    else if(p == INT64_C(2)) { result = b; result *= b; }
-    else if(p == INT64_C(3)) { result = b; result *= b; result *= b; }
-    else if(p == INT64_C(4)) { result = b; result *= b; result *= result; }
-    else
-    {
-        result = local_numeric_type(static_cast<unsigned>(UINT8_C(1)));
-
-        local_numeric_type y(b);
-
-        auto p_local = static_cast<std::uint64_t>(p);
-
-        // Use the so-called ladder method for the power calculation.
-        for(;;)
-        {
-            const auto do_power_multiply =
-              (static_cast<std::uint_fast8_t>(p_local & static_cast<unsigned>(UINT8_C(1))) != static_cast<std::uint_fast8_t>(UINT8_C(0)));
-
-            if(do_power_multiply)
-            {
-              result *= y;
-            }
-
-            p_local >>= static_cast<unsigned>(UINT8_C(1));
-
-            if(p_local == static_cast<std::uint64_t>(UINT8_C(0)))
-            {
-                break;
-            }
-
-            y *= y;
         }
     }
 
