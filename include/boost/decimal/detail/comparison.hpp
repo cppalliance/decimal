d30d80bdfe1caf08921133206eb568c0ef7df081
Matt Borland <matt@mattborland.com>
2023-10-23 09:04:07 +0200

    Fix overflow

--- include/boost/decimal/detail/comparison.hpp
+++ include/boost/decimal/detail/comparison.hpp
@@ -11,6 +11,7 @@
 #include <boost/decimal/detail/normalize.hpp>
 #include <boost/decimal/detail/promotion.hpp>
 #include <boost/decimal/detail/to_decimal.hpp>
+#include <boost/decimal/detail/shrink_significand.hpp>
 #include <boost/decimal/detail/cmath/isfinite.hpp>
 #include <cstdint>
 
@@ -21,20 +22,25 @@ template <typename DecimalType = decimal32, typename T1, typename T2>
 constexpr auto equal_parts_impl(T1 lhs_sig, std::int32_t lhs_exp, bool lhs_sign,
                                 T2 rhs_sig, std::int32_t rhs_exp, bool rhs_sign) noexcept -> bool
 {
-    detail::normalize<DecimalType>(lhs_sig, lhs_exp);
-    detail::normalize<DecimalType>(rhs_sig, rhs_exp);
+    using sig_type = std::conditional_t<std::is_same<DecimalType, decimal32>::value, std::uint32_t, std::uint64_t>;
+
+    auto new_lhs_sig {detail::shrink_significand<sig_type>(lhs_sig, lhs_exp)};
+    auto new_rhs_sig {detail::shrink_significand<sig_type>(rhs_sig, rhs_exp)};
+
+    detail::normalize<DecimalType>(new_lhs_sig, lhs_exp);
+    detail::normalize<DecimalType>(new_rhs_sig, rhs_exp);
 
     #ifdef BOOST_DECIMAL_DEBUG_EQUAL
     std::cerr << "Normalized Values"
-              << "\nlhs_sig: " << lhs_sig
+              << "\nlhs_sig: " << new_lhs_sig
               << "\nlhs_exp: " << lhs_exp
-              << "\nrhs_sig: " << rhs_sig
+              << "\nrhs_sig: " << new_rhs_sig
               << "\nrhs_exp: " << rhs_exp << std::endl;
     #endif
 
     return lhs_sign == rhs_sign &&
            lhs_exp == rhs_exp &&
-           lhs_sig == rhs_sig;
+           new_lhs_sig == new_rhs_sig;
 }
 
 template <typename Decimal, typename Integer>
@@ -103,18 +109,23 @@ constexpr auto less_parts_impl(T1 lhs_sig, std::int32_t lhs_exp, bool lhs_sign,
     const bool both_neg {lhs_sign && rhs_sign};
 
     // Normalize the significands and exponents
-    detail::normalize<DecimalType>(lhs_sig, lhs_exp);
-    detail::normalize<DecimalType>(rhs_sig, rhs_exp);
+    using sig_type = std::conditional_t<std::is_same<DecimalType, decimal32>::value, std::uint32_t, std::uint64_t>;
 
-    if (lhs_sig == 0 && rhs_sig != 0)
+    auto new_lhs_sig {detail::shrink_significand<sig_type>(lhs_sig, lhs_exp)};
+    auto new_rhs_sig {detail::shrink_significand<sig_type>(rhs_sig, rhs_exp)};
+
+    detail::normalize<DecimalType>(new_lhs_sig, lhs_exp);
+    detail::normalize<DecimalType>(new_rhs_sig, rhs_exp);
+
+    if (new_lhs_sig == 0 && new_rhs_sig != 0)
     {
         return (!rhs_sign);
     }
-    else if (lhs_sig != 0 && rhs_sig == 0)
+    else if (new_lhs_sig != 0 && new_rhs_sig == 0)
     {
         return lhs_sign;
     }
-    else if (lhs_sig == 0 && rhs_sig == 0)
+    else if (new_lhs_sig == 0 && new_rhs_sig == 0)
     {
         return false;
     }
@@ -130,12 +141,12 @@ constexpr auto less_parts_impl(T1 lhs_sig, std::int32_t lhs_exp, bool lhs_sign,
         }
         else
         {
-            return (lhs_sig > rhs_sig);
+            return (new_lhs_sig > new_rhs_sig);
         }
     }
     else
     {
-        if ((lhs_exp < rhs_exp) && (lhs_sig != static_cast<T1>(0)))
+        if ((lhs_exp < rhs_exp) && (new_lhs_sig != static_cast<T1>(0)))
         {
             return true;
         }
@@ -145,7 +156,7 @@ constexpr auto less_parts_impl(T1 lhs_sig, std::int32_t lhs_exp, bool lhs_sign,
         }
         else
         {
-            return (lhs_sig < rhs_sig);
+            return (new_lhs_sig < new_rhs_sig);
         }
     }
 }
