e46711daf4d7286561ea7eafb692ee43c9546557
Matt Borland <matt@mattborland.com>
2023-10-24 11:45:05 +0200

    Add basis

---
+++ include/boost/decimal/decimal128.hpp
@@ -0,0 +1,60 @@
+// Copyright 2023 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_DECIMAL_DECIMAL128_HPP
+#define BOOST_DECIMAL_DECIMAL128_HPP
+
+#include <cassert>
+#include <cerrno>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <cwchar>
+#include <iostream>
+#include <limits>
+#include <type_traits>
+#include <sstream>
+
+#include <boost/decimal/fwd.hpp>
+#include <boost/decimal/detail/attributes.hpp>
+#include <boost/decimal/detail/apply_sign.hpp>
+#include <boost/decimal/detail/bit_cast.hpp>
+#include <boost/decimal/detail/config.hpp>
+#include <boost/decimal/detail/emulated128.hpp>
+#include <boost/decimal/detail/emulated256.hpp>
+#include <boost/decimal/detail/fenv_rounding.hpp>
+#include <boost/decimal/detail/integer_search_trees.hpp>
+#include <boost/decimal/detail/parser.hpp>
+#include <boost/decimal/detail/power_tables.hpp>
+#include <boost/decimal/detail/ryu/ryu_generic_128.hpp>
+#include <boost/decimal/detail/type_traits.hpp>
+#include <boost/decimal/detail/utilities.hpp>
+#include <boost/decimal/detail/normalize.hpp>
+#include <boost/decimal/detail/comparison.hpp>
+#include <boost/decimal/detail/mixed_decimal_arithmetic.hpp>
+#include <boost/decimal/detail/to_integral.hpp>
+#include <boost/decimal/detail/to_float.hpp>
+#include <boost/decimal/detail/to_decimal.hpp>
+#include <boost/decimal/detail/promotion.hpp>
+#include <boost/decimal/detail/io.hpp>
+#include <boost/decimal/detail/check_non_finite.hpp>
+#include <boost/decimal/detail/shrink_significand.hpp>
+#include <boost/decimal/detail/cmath/isfinite.hpp>
+#include <boost/decimal/detail/cmath/fpclassify.hpp>
+#include <boost/decimal/detail/cmath/abs.hpp>
+#include <boost/decimal/detail/cmath/floor.hpp>
+#include <boost/decimal/detail/cmath/ceil.hpp>
+
+namespace boost {
+namespace decimal {
+
+namespace detail {
+
+}
+
+} //namespace decimal
+} //namespace boost
+
+#endif //BOOST_DECIMAL_DECIMAL128_HPP
