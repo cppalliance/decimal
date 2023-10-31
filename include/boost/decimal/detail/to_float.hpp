025d9d7070a9ede2f3484154b363d85b37d20802
Matt Borland <matt@mattborland.com>
2023-10-31 11:55:12 +0100

    Shrink significand for decimal128 calculations

--- include/boost/decimal/detail/to_float.hpp
+++ include/boost/decimal/detail/to_float.hpp
@@ -10,6 +10,7 @@
 #include <limits>
 #include <type_traits>
 #include <boost/decimal/detail/config.hpp>
+#include <boost/decimal/detail/shrink_significand.hpp>
 #include <boost/decimal/detail/fast_float/compute_float32.hpp>
 #include <boost/decimal/detail/fast_float/compute_float64.hpp>
 
@@ -41,16 +42,22 @@ BOOST_DECIMAL_CXX20_CONSTEXPR auto to_float(Decimal val) noexcept -> TargetType
 
     // The casts to result are redundant but in pre C++17 modes MSVC warns about implicit conversions
     TargetType result {};
+
+    auto sig = val.full_significand();
+    auto exp = val.biased_exponent();
+    const auto new_sig {detail::shrink_significand<std::uint64_t>(sig, exp)};
+
+
     BOOST_DECIMAL_IF_CONSTEXPR (std::is_same<TargetType, float>::value)
     {
-        result = static_cast<TargetType>(detail::fast_float::compute_float32(val.biased_exponent(),
-                                                                             val.full_significand(),
+        result = static_cast<TargetType>(detail::fast_float::compute_float32(exp,
+                                                                             new_sig,
                                                                              val.isneg(), success));
     }
     else BOOST_DECIMAL_IF_CONSTEXPR (std::is_same<TargetType, double>::value)
     {
-        result = static_cast<TargetType>(detail::fast_float::compute_float64(val.biased_exponent(),
-                                                                             val.full_significand(),
+        result = static_cast<TargetType>(detail::fast_float::compute_float64(exp,
+                                                                             new_sig,
                                                                              val.isneg(), success));
     }
     // TODO(mborland): Implement conversion to long double
