923ca3653e7b9af6cb45756f51388d5cd5aac302
Matt Borland <matt@mattborland.com>
2023-11-07 12:59:19 +0100

    Ignore MSVC warnings

--- include/boost/decimal/decimal128.hpp
+++ include/boost/decimal/decimal128.hpp
@@ -1041,6 +1041,11 @@ std::ostream& operator<<( std::ostream& os, boost::decimal::detail::uint128_t v 
 }
 #endif
 
+#ifdef _MSC_VER
+#  pragma warning(push)
+#  pragma warning(disable: 4127) // If constexpr macro only works for C++17 and above
+#endif
+
 template <typename T1, typename T2>
 constexpr auto d128_add_impl(T1 lhs_sig, std::int32_t lhs_exp, bool lhs_sign,
                              T2 rhs_sig, std::int32_t rhs_exp, bool rhs_sign) noexcept -> detail::decimal128_components
@@ -1198,6 +1203,10 @@ constexpr auto d128_sub_impl(T1 lhs_sig, std::int32_t lhs_exp, bool lhs_sign,
 
     return {res_sig, new_exp, new_sign};
 }
+
+#ifdef _MSC_VER
+#  pragma warning(pop)
+#endif
 
 constexpr auto operator+(decimal128 lhs, decimal128 rhs) noexcept -> decimal128
 {
