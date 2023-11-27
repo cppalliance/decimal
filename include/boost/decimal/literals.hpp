80ff01ae830bb5b8bad3e2651f4e7d0496d8c6a8
Matt Borland <matt@mattborland.com>
2023-11-27 11:59:12 +0100

    Add missing header

--- include/boost/decimal/literals.hpp
+++ include/boost/decimal/literals.hpp
@@ -8,6 +8,7 @@
 #include <boost/decimal/fwd.hpp>
 #include <boost/decimal/detail/config.hpp>
 #include <boost/decimal/detail/type_traits.hpp>
+#include <boost/decimal/cstdlib.hpp>
 
 #if !defined(BOOST_DECIMAL_DISABLE_CLIB)
 
