5ba178358c5b7fc2a02b6f453f1eeddb984ecd33
Matt Borland <matt@mattborland.com>
2023-11-28 10:21:09 +0100

    Fix division in ceil

--- include/boost/decimal/detail/cmath/ceil.hpp
+++ include/boost/decimal/detail/cmath/ceil.hpp
@@ -23,6 +23,9 @@ template <BOOST_DECIMAL_DECIMAL_FLOATING_TYPE T>
 constexpr auto ceil BOOST_DECIMAL_PREVENT_MACRO_SUBSTITUTION (T val) noexcept
     -> std::enable_if_t<detail::is_decimal_floating_point_v<T>, T>
 {
+    using DivType = std::conditional_t<std::is_same<T, decimal32>::value, std::uint32_t,
+                    std::conditional_t<std::is_same<T, decimal64>::value, std::uint64_t, detail::uint128>>;
+
     constexpr T zero {0, 0};
     constexpr T one {1, 0};
     const auto fp {fpclassify(val)};
@@ -58,7 +61,7 @@ constexpr auto ceil BOOST_DECIMAL_PREVENT_MACRO_SUBSTITUTION (T val) noexcept
         decimal_digits--;
     }
 
-    new_sig /= detail::pow10<std::uint32_t>(decimal_digits);
+    new_sig /= detail::pow10<DivType>(decimal_digits);
     if (!is_neg)
     {
         ++new_sig;
