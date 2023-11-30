35b5a5678071ae70f239c802b48a7279c6cad32f
Matt Borland <matt@mattborland.com>
2023-11-30 11:40:55 +0100

    Add evaluate polynomial tool

---
+++ include/boost/decimal/detail/cmath/impl/evaluate_polynomial.hpp
@@ -0,0 +1,38 @@
+//  (C) Copyright John Maddock 2006.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_DECIMAL_DETAIL_CMATH_IMPL_HPP
+#define BOOST_DECIMAL_DETAIL_CMATH_IMPL_HPP
+
+#include <boost/decimal/detail/config.hpp>
+
+namespace boost {
+namespace decimal {
+namespace tools {
+
+//
+// Polynomial evaluation with runtime size.
+// This requires a for-loop which may be more expensive than
+// the loop expanded versions above:
+//
+template <typename T, typename U>
+constexpr U evaluate_polynomial(const T* poly, U const& z, std::size_t count) noexcept
+{
+    BOOST_DECIMAL_ASSERT(count > 0);
+    auto sum = static_cast<U>(poly[count - 1]);
+    for(int i = static_cast<int>(count) - 2; i >= 0; --i)
+    {
+        sum *= z;
+        sum += static_cast<U>(poly[i]);
+    }
+
+    return sum;
+}
+
+} //namespace tools
+} //namespace decimal
+} //namespace boost
+
+#endif //BOOST_DECIMAL_DETAIL_CMATH_IMPL_HPP
