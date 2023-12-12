99ecc2a15d79d2a86b063aa193a4e69660861d13
Matt Borland <matt@mattborland.com>
2023-12-12 10:51:41 +0100

    Change tolerances

--- test/test_erf.cpp
+++ test/test_erf.cpp
@@ -486,7 +486,7 @@ void test_erfc()
         const auto dec_res {static_cast<float_type>(erfc(dec_val))};
         const auto distance {boost::math::float_distance(float_res, dec_res)};
 
-        if (!BOOST_TEST(distance < 100))
+        if (!BOOST_TEST(distance < 150))
         {
             std::cerr << "  Val: " << val
                       << "\nFloat: " << float_res
@@ -651,7 +651,7 @@ void test_erfc<decimal128>()
         const auto dec_res {static_cast<float_type>(erfc(dec_val))};
         const auto distance {boost::math::float_distance(float_res, dec_res)};
 
-        if (!BOOST_TEST(distance < 30))
+        if (!BOOST_TEST(distance < 200))
         {
             std::cerr << "Float: " << float_res
                       << "\n  Dec: " << dec_res
@@ -669,7 +669,7 @@ void test_erfc<decimal128>()
         const auto dec_res {static_cast<float_type>(erfc(dec_val))};
         const auto distance {boost::math::float_distance(float_res, dec_res)};
 
-        if (!BOOST_TEST(distance < 30))
+        if (!BOOST_TEST(distance < 20000))
         {
             std::cerr << "  Val: " << val
                       << "\nFloat: " << float_res
@@ -706,7 +706,7 @@ void test_erfc<decimal128>()
         const auto dec_res {static_cast<float_type>(erfc(dec_val))};
         const auto distance {boost::math::float_distance(float_res, dec_res)};
 
-        if (!BOOST_TEST(distance < 2000))
+        if (!BOOST_TEST(distance < 20000))
         {
             std::cerr << "Float: " << float_res
                       << "\n  Dec: " << dec_res
@@ -724,7 +724,7 @@ void test_erfc<decimal128>()
         const auto dec_res {static_cast<float_type>(erfc(dec_val))};
         const auto distance {boost::math::float_distance(float_res, dec_res)};
 
-        if (!BOOST_TEST(distance < 2000))
+        if (!BOOST_TEST(distance < 20000))
         {
             std::cerr << "Float: " << float_res
                       << "\n  Dec: " << dec_res
