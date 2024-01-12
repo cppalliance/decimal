f7cae56add886182758a531eac8f52570cf5d2df
Matt Borland <matt@mattborland.com>
2024-01-12 10:56:04 +0100

    Add impl for decimal128

--- include/boost/decimal/detail/cmath/impl/asin_impl.hpp
+++ include/boost/decimal/detail/cmath/impl/asin_impl.hpp
@@ -6,6 +6,8 @@
 #define BOOST_DECIMAL_DETAIL_CMATH_IMPL_ASIN_IMPL_HPP
 
 #include <boost/decimal/detail/concepts.hpp>
+#include <boost/decimal/detail/emulated128.hpp>
+#include <boost/decimal/detail/cmath/fma.hpp>
 #include <cstdint>
 
 namespace boost {
@@ -30,6 +32,40 @@ constexpr auto asin_impl(T x) noexcept
     constexpr T a8  {UINT64_C(25906093603686159), -22};
     constexpr T a9  {UINT64_C(99999996600828589), -17};
     constexpr T a10 {UINT64_C(73651618860008751), -27};
+
+    result = a0;
+    result = fma(result, x, a1);
+    result = fma(result, x, a2);
+    result = fma(result, x, a3);
+    result = fma(result, x, a4);
+    result = fma(result, x, a5);
+    result = fma(result, x, a6);
+    result = fma(result, x, a7);
+    result = fma(result, x, a8);
+    result = fma(result, x, a9);
+    result = fma(result, x, a10);
+
+    return result;
+}
+
+template <>
+constexpr auto asin_impl<decimal128>(decimal128 x) noexcept
+{
+    decimal128 result {};
+
+    // 10th degree remez polynomial calculated from 0, 0.5
+    // Estimated max error: 7.36516188600087509860029869041014e-11
+    constexpr decimal128 a0  {uint128{UINT64_C(143010115336237), UINT64_C(516409510824292249)}, -34};
+    constexpr decimal128 a1  {uint128{UINT64_C(235767315028868), UINT64_C(18335361300665096842)}, -34, true};
+    constexpr decimal128 a2  {uint128{UINT64_C(165661130571303), UINT64_C(2696222372090233442)}, -34};
+    constexpr decimal128 a3  {uint128{UINT64_C(75770177096626), UINT64_C(1136186724396506434)}, -34, true};
+    constexpr decimal128 a4  {uint128{UINT64_C(295841983277201), UINT64_C(3737404419660904404)}, -35};
+    constexpr decimal128 a5  {uint128{UINT64_C(347225199319988), UINT64_C(12011146057825411692)}, -35};
+    constexpr decimal128 a6  {uint128{UINT64_C(62920056131937), UINT64_C(2964129682239744913)}, -36};
+    constexpr decimal128 a7  {uint128{UINT64_C(90265192507970), UINT64_C(4360833744321461000)}, -34};
+    constexpr decimal128 a8  {uint128{UINT64_C(140437214828213), UINT64_C(2679609872435210522)}, -39};
+    constexpr decimal128 a9  {uint128{UINT64_C(542101067814679), UINT64_C(9115059183029878256)}, -34};
+    constexpr decimal128 a10 {uint128{UINT64_C(399266221538670), UINT64_C(9343090405564050290)}, -44};
 
     result = a0;
     result = fma(result, x, a1);
