7db0bfa5fff9aaa7ab47993b159b60fab47d24d0
Christopher Kormanyos <ckormanyos@yahoo.com>
2024-01-18 06:20:36 +0100

    Put debug messages in test_atan2.cpp

--- test/test_atan2.cpp
+++ test/test_atan2.cpp
@@ -8,9 +8,11 @@
 #include <boost/decimal.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <boost/math/special_functions/next.hpp>
+
+#include <cmath>
 #include <iostream>
 #include <random>
-#include <cmath>
+#include <sstream>
 
 #if !defined(BOOST_DECIMAL_REDUCE_TEST_DEPTH)
 static constexpr auto N = static_cast<std::size_t>(128U); // Number of trials
@@ -68,8 +70,56 @@ void test()
     BOOST_TEST_EQ(atan2(-std::numeric_limits<Dec>::infinity(), Dec{one(rng)}), -numbers::pi_v<Dec> / 2);
     BOOST_TEST_EQ(atan2(std::numeric_limits<Dec>::infinity(), -std::numeric_limits<Dec>::infinity()), 3 * one(rng) * numbers::pi_v<Dec> / 4);
     BOOST_TEST_EQ(atan2(-std::numeric_limits<Dec>::infinity(), -std::numeric_limits<Dec>::infinity()), -3 * one(rng) * numbers::pi_v<Dec> / 4);
-    BOOST_TEST_EQ(atan2(std::numeric_limits<Dec>::infinity(), std::numeric_limits<Dec>::infinity()), one(rng) * numbers::pi_v<Dec> / 4);
-    BOOST_TEST_EQ(atan2(-std::numeric_limits<Dec>::infinity(), std::numeric_limits<Dec>::infinity()), -one(rng) * numbers::pi_v<Dec> / 4);
+
+    const auto one_rng_pos   = one(rng);
+    const auto atan2_inf_pos = atan2(std::numeric_limits<Dec>::infinity(), std::numeric_limits<Dec>::infinity());
+    const auto ctrl_inf_pos  = one_rng_pos * numbers::pi_v<Dec> / 4;
+    BOOST_TEST_EQ(atan2_inf_pos, ctrl_inf_pos);
+
+    const auto one_rng_neg = -one(rng);
+    const auto atan2_inf_neg = atan2(-std::numeric_limits<Dec>::infinity(), std::numeric_limits<Dec>::infinity());
+    const auto ctrl_inf_neg  = one_rng_neg * numbers::pi_v<Dec> / 4;
+    BOOST_TEST_EQ(atan2_inf_neg, ctrl_inf_neg);
+
+    if(sizeof(Dec) >= 8U)
+    {
+        {
+            std::stringstream strm;
+
+            // Chris hex-dump:
+            strm << "Chris hex-dump (atan2_inf_pos, ctrl_inf_pos): ("
+                 << std::uppercase
+                 << std::noshowbase
+                 << std::hex
+                 << std::setw(16)
+                 << std::setfill('0')
+                 << *reinterpret_cast<const std::uint64_t*>(&atan2_inf_pos)
+                 << ", "
+                 << *reinterpret_cast<const std::uint64_t*>(&ctrl_inf_pos)
+                 << ")";
+
+            std::cout << strm.str() << std::endl;
+        }
+
+        {
+            std::stringstream strm;
+
+            // Chris hex-dump:
+            strm << "Chris hex-dump (atan2_inf_neg, ctrl_inf_neg): ("
+                 << std::uppercase
+                 << std::noshowbase
+                 << std::hex
+                 << std::setw(16)
+                 << std::setfill('0')
+                 << *reinterpret_cast<const std::uint64_t*>(&atan2_inf_neg)
+                 << ", "
+                 << *reinterpret_cast<const std::uint64_t*>(&ctrl_inf_neg)
+                 << ")";
+
+            std::cout << strm.str() << std::endl;
+        }
+    }
+
     BOOST_TEST_EQ(atan2(-Dec(1), Dec{0 * one(rng)}), -numbers::pi_v<Dec> / 2);
     BOOST_TEST_EQ(atan2(Dec(1), Dec{0 * one(rng)}), numbers::pi_v<Dec> / 2);
     BOOST_TEST_EQ(atan2(-Dec{one(rng)}, -std::numeric_limits<Dec>::infinity()), -numbers::pi_v<Dec>);
