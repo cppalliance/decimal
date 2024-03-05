9fddb562b5d14a5141aaa48c702d0e5b6b9dc910
Matt Borland <matt@mattborland.com>
2024-03-05 11:59:29 +0100

    Move laurent series path into struct

--- include/boost/decimal/detail/cmath/impl/lgamma_impl.hpp
+++ include/boost/decimal/detail/cmath/impl/lgamma_impl.hpp
@@ -93,6 +93,78 @@ struct lgamma_taylor_series_imp
     // TODO: add d128_coeffs
 };
 
+template <bool b>
+struct lgamma_laurent_series_imp
+{
+    static constexpr std::array<decimal32, 26> d32_coeffs = {{
+        // Use a Laurent infinite-series expansion of the logarithm of the
+        // gamma function divided by the square root.
+        // N[Series[Log[Gamma[x]], {x, Infinity, 26}], 32]
+        // log( e^-x * x^x * sqrt(x) * [Series below] )
+        // See also Wolfram Alpha(R): https://www.wolframalpha.com/input?i=Series%5BLog%5BGamma%5Bx%5D%5D%2C+%7Bx%2C+Infinity%2C+4%7D%5D
+          decimal32 { UINT64_C(250'662'827'463'100'050), + 1 - 18 }, // * x^-1
+        + decimal32 { UINT64_C(208'885'689'552'583'375), - 0 - 18 }, // * x^-2
+        + decimal32 { UINT64_C(870'357'039'802'430'730), - 2 - 18 }, // * x^-3
+        - decimal32 { UINT64_C(672'109'047'402'988'175), - 2 - 18 }, // * x^-4
+        - decimal32 { UINT64_C(575'201'238'110'171'235), - 3 - 18 }, // * x^-5
+        + decimal32 { UINT64_C(196'529'488'158'320'306), - 2 - 18 }, // * x^-6
+        + decimal32 { UINT64_C(174'782'521'204'559'121), - 3 - 18 }, // * x^-7
+        - decimal32 { UINT64_C(148'434'113'515'827'614), - 2 - 18 }, // * x^-8
+        - decimal32 { UINT64_C(129'637'573'211'255'432), - 3 - 18 }, // * x^-9
+        + decimal32 { UINT64_C(210'431'122'975'320'637), - 2 - 18 }, // * x^-10
+        + decimal32 { UINT64_C(180'599'945'655'550'436), - 3 - 18 }, // * x^-11
+        - decimal32 { UINT64_C(479'878'567'054'634'606), - 2 - 18 }, // * x^-12
+        - decimal32 { UINT64_C(407'367'859'381'525'183), - 3 - 18 }, // * x^-13
+        + decimal32 { UINT64_C(160'508'503'319'445'960), - 1 - 18 }, // * x^-14
+        + decimal32 { UINT64_C(135'399'228'015'909'411), - 2 - 18 }, // * x^-15
+        - decimal32 { UINT64_C(740'154'212'684'273'819), - 1 - 18 }, // * x^-16
+        - decimal32 { UINT64_C(622'080'867'880'877'866), - 2 - 18 }, // * x^-17
+        + decimal32 { UINT64_C(450'040'333'856'250'984), - 0 - 18 }, // * x^-18
+        + decimal32 { UINT64_C(377'400'786'521'707'440), - 1 - 18 }, // * x^-19
+        - decimal32 { UINT64_C(348'872'797'304'123'310), + 1 - 18 }, // * x^-20
+        - decimal32 { UINT64_C(292'138'192'227'179'792), - 0 - 18 }, // * x^-21
+        + decimal32 { UINT64_C(335'837'691'649'553'084), + 2 - 18 }, // * x^-22
+        + decimal32 { UINT64_C(280'944'016'052'174'395), + 1 - 18 }, // * x^-23
+        - decimal32 { UINT64_C(393'042'854'585'987'930), + 3 - 18 }, // * x^-24
+        - decimal32 { UINT64_C(328'565'400'725'242'178), + 2 - 18 }, // * x^-25
+        + decimal32 { UINT64_C(549'592'170'046'323'711), + 4 - 18 }, // * x^-26
+    }};
+    
+    static constexpr std::array<decimal64, 26> d64_coeffs = {{
+        // Use a Laurent infinite-series expansion of the logarithm of the
+        // gamma function divided by the square root.
+        // N[Series[Log[Gamma[x]], {x, Infinity, 26}], 32]
+        // log( e^-x * x^x * sqrt(x) * [Series below] )
+        // See also Wolfram Alpha(R): https://www.wolframalpha.com/input?i=Series%5BLog%5BGamma%5Bx%5D%5D%2C+%7Bx%2C+Infinity%2C+4%7D%5D
+          decimal64 { UINT64_C(250'662'827'463'100'050), + 1 - 18 }, // * x^-1
+        + decimal64 { UINT64_C(208'885'689'552'583'375), - 0 - 18 }, // * x^-2
+        + decimal64 { UINT64_C(870'357'039'802'430'730), - 2 - 18 }, // * x^-3
+        - decimal64 { UINT64_C(672'109'047'402'988'175), - 2 - 18 }, // * x^-4
+        - decimal64 { UINT64_C(575'201'238'110'171'235), - 3 - 18 }, // * x^-5
+        + decimal64 { UINT64_C(196'529'488'158'320'306), - 2 - 18 }, // * x^-6
+        + decimal64 { UINT64_C(174'782'521'204'559'121), - 3 - 18 }, // * x^-7
+        - decimal64 { UINT64_C(148'434'113'515'827'614), - 2 - 18 }, // * x^-8
+        - decimal64 { UINT64_C(129'637'573'211'255'432), - 3 - 18 }, // * x^-9
+        + decimal64 { UINT64_C(210'431'122'975'320'637), - 2 - 18 }, // * x^-10
+        + decimal64 { UINT64_C(180'599'945'655'550'436), - 3 - 18 }, // * x^-11
+        - decimal64 { UINT64_C(479'878'567'054'634'606), - 2 - 18 }, // * x^-12
+        - decimal64 { UINT64_C(407'367'859'381'525'183), - 3 - 18 }, // * x^-13
+        + decimal64 { UINT64_C(160'508'503'319'445'960), - 1 - 18 }, // * x^-14
+        + decimal64 { UINT64_C(135'399'228'015'909'411), - 2 - 18 }, // * x^-15
+        - decimal64 { UINT64_C(740'154'212'684'273'819), - 1 - 18 }, // * x^-16
+        - decimal64 { UINT64_C(622'080'867'880'877'866), - 2 - 18 }, // * x^-17
+        + decimal64 { UINT64_C(450'040'333'856'250'984), - 0 - 18 }, // * x^-18
+        + decimal64 { UINT64_C(377'400'786'521'707'440), - 1 - 18 }, // * x^-19
+        - decimal64 { UINT64_C(348'872'797'304'123'310), + 1 - 18 }, // * x^-20
+        - decimal64 { UINT64_C(292'138'192'227'179'792), - 0 - 18 }, // * x^-21
+        + decimal64 { UINT64_C(335'837'691'649'553'084), + 2 - 18 }, // * x^-22
+        + decimal64 { UINT64_C(280'944'016'052'174'395), + 1 - 18 }, // * x^-23
+        - decimal64 { UINT64_C(393'042'854'585'987'930), + 3 - 18 }, // * x^-24
+        - decimal64 { UINT64_C(328'565'400'725'242'178), + 2 - 18 }, // * x^-25
+        + decimal64 { UINT64_C(549'592'170'046'323'711), + 4 - 18 }, // * x^-26
+    }};
+};
+
 #if !(defined(__cpp_inline_variables) && __cpp_inline_variables >= 201606L) && (!defined(_MSC_VER) || _MSC_VER != 1900)
 
 template <bool b>
@@ -101,11 +173,18 @@ constexpr std::array<decimal32, 25> lgamma_taylor_series_imp<b>::d32_coeffs;
 template <bool b>
 constexpr std::array<decimal64, 25> lgamma_taylor_series_imp<b>::d64_coeffs;
 
+template <bool b>
+constexpr std::array<decimal32, 26> lgamma_laurent_series_imp<b>::d32_coeffs;
+
+template <bool b>
+constexpr std::array<decimal64, 26> lgamma_laurent_series_imp<b>::d64_coeffs;
+
 #endif
 
 } //namespace lgamma_detail
 
 using lgamma_taylor_series_table = lgamma_detail::lgamma_taylor_series_imp<true>;
+using lgamma_laurent_series_table = lgamma_detail::lgamma_laurent_series_imp<true>;
 
 template <BOOST_DECIMAL_DECIMAL_FLOATING_TYPE T>
 constexpr auto lgamma_taylor_series_expansion(T x) noexcept
@@ -127,6 +206,28 @@ template <>
 constexpr auto lgamma_taylor_series_expansion<decimal64>(decimal64 x) noexcept
 {
     return taylor_series_result(x, lgamma_taylor_series_table::d64_coeffs);
+}
+
+template <BOOST_DECIMAL_DECIMAL_FLOATING_TYPE T>
+constexpr auto lgamma_laurent_series_expansion(T x) noexcept
+{
+    // LCOV_EXCL_START
+    static_assert(!std::is_same<T, decimal128>::value, "Decimal128 has not been implemented");
+    static_cast<void>(x);
+    return T{1,0,true};
+    // LCOV_EXCL_STOP
+}
+
+template <>
+constexpr auto lgamma_laurent_series_expansion<decimal32>(decimal32 x) noexcept
+{
+    return taylor_series_result(x, lgamma_laurent_series_table::d32_coeffs);
+}
+
+template <>
+constexpr auto lgamma_laurent_series_expansion<decimal64>(decimal64 x) noexcept
+{
+    return taylor_series_result(x, lgamma_laurent_series_table::d64_coeffs);
 }
 
 } //namespace detail
--- include/boost/decimal/detail/cmath/lgamma.hpp
+++ include/boost/decimal/detail/cmath/lgamma.hpp
@@ -91,57 +91,11 @@ constexpr auto lgamma(T x) noexcept -> std::enable_if_t<detail::is_decimal_float
             }
             else
             {
-                using coefficient_array_type = std::array<T, static_cast<std::size_t>(UINT8_C(26))>;
-
-                #if (defined(__clang__) && (__clang__ < 6))
-                #  pragma clang diagnostic push
-                #  pragma clang diagnostic ignored "-Wmissing-braces"
-                #endif
-
-                constexpr auto coefficient_table =
-                    coefficient_array_type
-                    {
-                        // Use a Laurent infinite-series expansion of the logarithm of the
-                        // gamma function divided by the square root.
-                        // N[Series[Log[Gamma[x]], {x, Infinity, 26}], 32]
-                        // log( e^-x * x^x * sqrt(x) * [Series below] )
-                        // See also Wolfram Alpha(R): https://www.wolframalpha.com/input?i=Series%5BLog%5BGamma%5Bx%5D%5D%2C+%7Bx%2C+Infinity%2C+4%7D%5D
-                          T { UINT64_C(250'662'827'463'100'050), + 1 - 18 }, // * x^-1
-                        + T { UINT64_C(208'885'689'552'583'375), - 0 - 18 }, // * x^-2
-                        + T { UINT64_C(870'357'039'802'430'730), - 2 - 18 }, // * x^-3
-                        - T { UINT64_C(672'109'047'402'988'175), - 2 - 18 }, // * x^-4
-                        - T { UINT64_C(575'201'238'110'171'235), - 3 - 18 }, // * x^-5
-                        + T { UINT64_C(196'529'488'158'320'306), - 2 - 18 }, // * x^-6
-                        + T { UINT64_C(174'782'521'204'559'121), - 3 - 18 }, // * x^-7
-                        - T { UINT64_C(148'434'113'515'827'614), - 2 - 18 }, // * x^-8
-                        - T { UINT64_C(129'637'573'211'255'432), - 3 - 18 }, // * x^-9
-                        + T { UINT64_C(210'431'122'975'320'637), - 2 - 18 }, // * x^-10
-                        + T { UINT64_C(180'599'945'655'550'436), - 3 - 18 }, // * x^-11
-                        - T { UINT64_C(479'878'567'054'634'606), - 2 - 18 }, // * x^-12
-                        - T { UINT64_C(407'367'859'381'525'183), - 3 - 18 }, // * x^-13
-                        + T { UINT64_C(160'508'503'319'445'960), - 1 - 18 }, // * x^-14
-                        + T { UINT64_C(135'399'228'015'909'411), - 2 - 18 }, // * x^-15
-                        - T { UINT64_C(740'154'212'684'273'819), - 1 - 18 }, // * x^-16
-                        - T { UINT64_C(622'080'867'880'877'866), - 2 - 18 }, // * x^-17
-                        + T { UINT64_C(450'040'333'856'250'984), - 0 - 18 }, // * x^-18
-                        + T { UINT64_C(377'400'786'521'707'440), - 1 - 18 }, // * x^-19
-                        - T { UINT64_C(348'872'797'304'123'310), + 1 - 18 }, // * x^-20
-                        - T { UINT64_C(292'138'192'227'179'792), - 0 - 18 }, // * x^-21
-                        + T { UINT64_C(335'837'691'649'553'084), + 2 - 18 }, // * x^-22
-                        + T { UINT64_C(280'944'016'052'174'395), + 1 - 18 }, // * x^-23
-                        - T { UINT64_C(393'042'854'585'987'930), + 3 - 18 }, // * x^-24
-                        - T { UINT64_C(328'565'400'725'242'178), + 2 - 18 }, // * x^-25
-                        + T { UINT64_C(549'592'170'046'323'711), + 4 - 18 }, // * x^-26
-                    };
-
-                #if (defined(__clang__) && (__clang__ < 6))
-                #  pragma clang diagnostic pop
-                #endif
 
                 // Perform the Laurent series expansion.
                 const auto one_over_x = one / x;
 
-                result = detail::taylor_series_result(one_over_x, coefficient_table);
+                result = detail::lgamma_laurent_series_expansion(one_over_x);
 
                 result = (x * (log(x) - one)) + log(result / sqrt(x));
             }
