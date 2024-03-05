104493caaea584d06bfc8a3bb0453a6b79dcec49
Matt Borland <matt@mattborland.com>
2024-03-05 08:22:42 +0100

    Add ODR violation test case

---
+++ test/link_2.cpp
@@ -0,0 +1,20 @@
+// Copyright 2024 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/decimal.hpp>
+
+void test_odr_use(const boost::decimal::rounding_mode*);
+
+template <typename T>
+void test()
+{
+    test_odr_use(&boost::decimal::_boost_decimal_global_rounding_mode);
+}
+
+void f2()
+{
+    test<boost::decimal::decimal32>();
+    test<boost::decimal::decimal64>();
+    test<boost::decimal::decimal128>();
+}
