6353000d17d5daf7e1067a644ef58834c38c8ea2
Matt Borland <matt@mattborland.com>
2024-03-15 12:12:56 +0100

    Add general format specifier to tests

--- test/test_format.cpp
+++ test/test_format.cpp
@@ -31,6 +31,24 @@ void test_general()
     BOOST_TEST_EQ(std::format("{}", -std::numeric_limits<T>::quiet_NaN()), "-nan(ind)");
     BOOST_TEST_EQ(std::format("{}", std::numeric_limits<T>::signaling_NaN()), "nan(snan)");
     BOOST_TEST_EQ(std::format("{}", -std::numeric_limits<T>::signaling_NaN()), "-nan(snan)");
+
+    BOOST_TEST_EQ(std::format("{:g}", T{1}), "1");
+    BOOST_TEST_EQ(std::format("{:g}", T{10}), "10");
+    BOOST_TEST_EQ(std::format("{:g}", T{100}), "100");
+    BOOST_TEST_EQ(std::format("{:g}", T{1000}), "1000");
+    BOOST_TEST_EQ(std::format("{:g}", T{10000}), "10000");
+    BOOST_TEST_EQ(std::format("{:g}", T{210000}), "210000");
+    BOOST_TEST_EQ(std::format("{:g}", T{2100000}), "2100000");
+    BOOST_TEST_EQ(std::format("{:g}", T{21, 6, true}), "-2.1e+07");
+    BOOST_TEST_EQ(std::format("{:g}", T{211, 6, true}), "-2.11e+08");
+    BOOST_TEST_EQ(std::format("{:g}", T{2111, 6, true}), "-2.111e+09");
+
+    BOOST_TEST_EQ(std::format("{:g}", std::numeric_limits<T>::infinity()), "inf");
+    BOOST_TEST_EQ(std::format("{:g}", -std::numeric_limits<T>::infinity()), "-inf");
+    BOOST_TEST_EQ(std::format("{:g}", std::numeric_limits<T>::quiet_NaN()), "nan");
+    BOOST_TEST_EQ(std::format("{:g}", -std::numeric_limits<T>::quiet_NaN()), "-nan(ind)");
+    BOOST_TEST_EQ(std::format("{:g}", std::numeric_limits<T>::signaling_NaN()), "nan(snan)");
+    BOOST_TEST_EQ(std::format("{:g}", -std::numeric_limits<T>::signaling_NaN()), "-nan(snan)");
 }
 
 int main()
