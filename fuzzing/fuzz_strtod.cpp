755311ccafe726e4114e3c54cb8700b494bbe6e7
Matt Borland <matt@mattborland.com>
2024-04-12 11:43:55 +0200

    Ensure null termination into strtod

--- fuzzing/fuzz_strtod.cpp
+++ fuzzing/fuzz_strtod.cpp
@@ -5,20 +5,22 @@
 #include <boost/decimal.hpp>
 #include <iostream>
 #include <exception>
+#include <string>
 
 extern "C" int LLVMFuzzerTestOneInput(const std::uint8_t* data, std::size_t size)
 {
     try
     {
         auto c_data = reinterpret_cast<const char*>(data);
+        std::string c_data_str {c_data, size}; // Guarantee null termination since we can't pass the size argument
 
         char** endptr = nullptr;
 
-        boost::decimal::strtod32(c_data, endptr);
+        boost::decimal::strtod32(c_data_str.c_str(), endptr);
 
-        boost::decimal::strtod64(c_data, endptr);
+        boost::decimal::strtod64(c_data_str.c_str(), endptr);
 
-        boost::decimal::strtod128(c_data, endptr);
+        boost::decimal::strtod128(c_data_str.c_str(), endptr);
     }
     catch(...)
     {
