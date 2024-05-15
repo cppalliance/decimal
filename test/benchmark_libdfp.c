d03fa698bfb9735894bc5c379abbc35038233702
Matt Borland <matt@mattborland.com>
2024-05-15 16:59:21 +0200

    Add _Decimal64 basic functions

--- test/benchmark_libdfp.c
+++ test/benchmark_libdfp.c
@@ -53,7 +53,7 @@ void test_comparisons_32(_Decimal32* data, const char* label)
     clock_gettime(CLOCK_MONOTONIC, &t2);
 
     uint64_t elapsed_time_us = (uint64_t)((t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_nsec - t1.tv_nsec) / 1000);
-    printf("comparisons<%-10s >: %-10" PRIu64 " us (s=%zu)\n", label, elapsed_time_us, s);
+    printf("Comparisons    <%-10s >: %-10" PRIu64 " us (s=%zu)\n", label, elapsed_time_us, s);
 }
 
 void generate_vector_64(_Decimal64* buffer, size_t buffer_len)
@@ -92,7 +92,7 @@ void test_comparisons_64(_Decimal64* data, const char* label)
     clock_gettime(CLOCK_MONOTONIC, &t2);
 
     uint64_t elapsed_time_us = (uint64_t)((t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_nsec - t1.tv_nsec) / 1000);
-    printf("comparisons<%-10s >: %-10" PRIu64 " us (s=%zu)\n", label, elapsed_time_us, s);
+    printf("Comparisons    <%-10s >: %-10" PRIu64 " us (s=%zu)\n", label, elapsed_time_us, s);
 }
 
 void generate_vector_128(_Decimal128* buffer, size_t buffer_len)
@@ -131,7 +131,7 @@ void test_comparisons_128(_Decimal128* data, const char* label)
     clock_gettime(CLOCK_MONOTONIC, &t2);
 
     uint64_t elapsed_time_us = (uint64_t)((t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_nsec - t1.tv_nsec) / 1000);
-    printf("comparisons<%-10s>: %-10" PRIu64 " us (s=%zu)\n", label, elapsed_time_us, s);
+    printf("Comparisons    <%-10s>: %-10" PRIu64 " us (s=%zu)\n", label, elapsed_time_us, s);
 }
 
 typedef _Decimal32 (*operation_32)(_Decimal32, _Decimal32);
@@ -152,10 +152,10 @@ _Decimal32 mul_32(_Decimal32 a, _Decimal32 b)
 
 _Decimal32 div_32(_Decimal32 a, _Decimal32 b)
 {
-    return a * b;
+    return a / b;
 }
 
-void test_two_element_operation_32(_Decimal32* data, operation_32 op, const char* label)
+void test_two_element_operation_32(_Decimal32* data, operation_32 op, const char* label, const char* op_label)
 {
     struct timespec t1, t2;
     clock_gettime(CLOCK_MONOTONIC, &t1);
@@ -176,7 +176,52 @@ void test_two_element_operation_32(_Decimal32* data, operation_32 op, const char
     clock_gettime(CLOCK_MONOTONIC, &t2);
 
     uint64_t elapsed_time_us = (uint64_t)((t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_nsec - t1.tv_nsec) / 1000);
-    printf("comparisons<%-10s>: %-10" PRIu64 " us (s=%zu)\n", label, elapsed_time_us, s);
+    printf("%-15s<%-10s >: %-10" PRIu64 " us (s=%zu)\n", op_label, label, elapsed_time_us, s);
+}
+
+typedef _Decimal64 (*operation_64)(_Decimal64, _Decimal64);
+
+_Decimal64 add_64(_Decimal64 a, _Decimal64 b)
+{
+    return a + b;
+}
+_Decimal64 sub_64(_Decimal64 a, _Decimal64 b)
+{
+    return a - b;
+}
+
+_Decimal64 mul_64(_Decimal64 a, _Decimal64 b)
+{
+    return a * b;
+}
+
+_Decimal64 div_64(_Decimal64 a, _Decimal64 b)
+{
+    return a / b;
+}
+
+void test_two_element_operation_64(_Decimal64* data, operation_64 op, const char* label, const char* op_label)
+{
+    struct timespec t1, t2;
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+    
+    size_t s = 0;
+
+    for (size_t n = 0; n < N; ++n)
+    {
+        for (size_t k = 0; k < K - 1; ++k)
+        {
+            _Decimal64 val1 = data[k];
+            _Decimal64 val2 = data[k + 1];
+
+            s += (size_t)op(val1, val2);
+        }
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &t2);
+
+    uint64_t elapsed_time_us = (uint64_t)((t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_nsec - t1.tv_nsec) / 1000);
+    printf("%-15s<%-10s >: %-10" PRIu64 " us (s=%zu)\n", op_label, label, elapsed_time_us, s);
 }
 
 int main()
@@ -206,19 +251,23 @@ int main()
     
     printf("\n===== Addition =====\n");
 
-    test_two_element_operation_32(d32_array, add_32, "_Decimal32");
+    test_two_element_operation_32(d32_array, add_32, "_Decimal32", "Addition");
+    test_two_element_operation_64(d64_array, add_64, "_Decimal64", "Addition");
 
     printf("\n===== Subtraction =====\n");
 
-    test_two_element_operation_32(d32_array, sub_32, "_Decimal32");
+    test_two_element_operation_32(d32_array, sub_32, "_Decimal32", "Subtraction");
+    test_two_element_operation_64(d64_array, sub_64, "_Decimal64", "Subtraction");
 
     printf("\n===== Multiplication =====\n");
 
-    test_two_element_operation_32(d32_array, mul_32, "_Decimal32");
+    test_two_element_operation_32(d32_array, mul_32, "_Decimal32", "Multiplication");
+    test_two_element_operation_64(d64_array, mul_64, "_Decimal64", "Multiplication");
 
     printf("\n===== Division =====\n");
 
-    test_two_element_operation_32(d32_array, div_32, "_Decimal32");
+    test_two_element_operation_32(d32_array, div_32, "_Decimal32", "Division");
+    test_two_element_operation_64(d64_array, div_64, "_Decimal64", "Division");
 
     free(d32_array);
     free(d64_array);
