10d85909764800f96d5fa359a17aad73f7cf147a
Matt Borland <matt@mattborland.com>
2024-05-15 11:26:07 +0200

    Fix potential overflows in conversions to integral types

--- include/boost/decimal/detail/to_integral.hpp
+++ include/boost/decimal/detail/to_integral.hpp
@@ -51,7 +51,12 @@ constexpr auto to_integral(Decimal val) noexcept
 
     auto result {static_cast<Conversion_Type>(val.full_significand())};
     auto expval {val.biased_exponent()};
-    if (expval > 0)
+
+    if (std::abs(expval) >= 19)
+    {
+        result = 0;
+    }
+    else if (expval > 0)
     {
         result *= detail::pow10<Conversion_Type>(static_cast<Conversion_Type>(expval));
     }
@@ -88,7 +93,12 @@ constexpr auto to_integral_128(Decimal val) noexcept
 
     auto sig {val.full_significand()};
     auto expval {val.biased_exponent()};
-    if (expval > 0)
+
+    if (std::abs(expval) >= 38)
+    {
+        sig = 0;
+    }
+    else if (expval > 0)
     {
         sig *= detail::pow10<detail::uint128>(expval);
     }
