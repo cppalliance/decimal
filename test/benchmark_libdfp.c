99b9fdf9ca2f6c57d22de79e0d965c42d8f8c672
Matt Borland <matt@mattborland.com>
2024-05-15 16:50:59 +0200

    Add basic functions for _Decimal32

--- test/benchmark_libdfp.c
+++ test/benchmark_libdfp.c
@@ -134,6 +134,51 @@ void test_comparisons_128(_Decimal128* data, const char* label)
     printf("comparisons<%-10s>: %-10" PRIu64 " us (s=%zu)\n", label, elapsed_time_us, s);
 }
 
+typedef _Decimal32 (*operation_32)(_Decimal32, _Decimal32);
+
+_Decimal32 add_32(_Decimal32 a, _Decimal32 b)
+{
+    return a + b;
+}
+_Decimal32 sub_32(_Decimal32 a, _Decimal32 b)
+{
+    return a - b;
+}
+
+_Decimal32 mul_32(_Decimal32 a, _Decimal32 b)
+{
+    return a * b;
+}
+
+_Decimal32 div_32(_Decimal32 a, _Decimal32 b)
+{
+    return a * b;
+}
+
+void test_two_element_operation_32(_Decimal32* data, operation_32 op, const char* label)
+{
+    struct timespec t1, t2;
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+    
+    size_t s = 0;
+
+    for (size_t n = 0; n < N; ++n)
+    {
+        for (size_t k = 0; k < K - 1; ++k)
+        {
+            _Decimal32 val1 = data[k];
+            _Decimal32 val2 = data[k + 1];
+
+            s += (size_t)op(val1, val2);
+        }
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &t2);
+
+    uint64_t elapsed_time_us = (uint64_t)((t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_nsec - t1.tv_nsec) / 1000);
+    printf("comparisons<%-10s>: %-10" PRIu64 " us (s=%zu)\n", label, elapsed_time_us, s);
+}
+
 int main()
 {
     // One time init of random number generator
@@ -148,6 +193,8 @@ int main()
         return 1;
     }
 
+    printf("===== Comparisons =====\n");
+
     generate_vector_32(d32_array, K);
     test_comparisons_32(d32_array, "_Decimal32");
 
@@ -157,6 +204,22 @@ int main()
     generate_vector_128(d128_array, K);
     test_comparisons_128(d128_array, "_Decimal128");
     
+    printf("\n===== Addition =====\n");
+
+    test_two_element_operation_32(d32_array, add_32, "_Decimal32");
+
+    printf("\n===== Subtraction =====\n");
+
+    test_two_element_operation_32(d32_array, sub_32, "_Decimal32");
+
+    printf("\n===== Multiplication =====\n");
+
+    test_two_element_operation_32(d32_array, mul_32, "_Decimal32");
+
+    printf("\n===== Division =====\n");
+
+    test_two_element_operation_32(d32_array, div_32, "_Decimal32");
+
     free(d32_array);
     free(d64_array);
     free(d128_array);
