53aa3eeb928155a87d0160cfaea2d5d6fec4ab6b
ckormanyos <ckormanyos@yahoo.com>
2024-06-13 06:57:22 +0200

    Correct a typo from merge conflict

--- include/boost/decimal/detail/cmath/impl/atan_impl.hpp
+++ include/boost/decimal/detail/cmath/impl/atan_impl.hpp
@@ -67,13 +67,6 @@ struct atan_table_imp
          ::boost::decimal::decimal128_fast { boost::decimal::detail::uint128 { UINT64_C(532773544924935), UINT64_C(16408933314882201700) }, -34 }, // atan_three_halves
     }};
 
-    static constexpr std::array<::boost::decimal::decimal128, 3> d128_atan_values =
-    {{
-        ::boost::decimal::decimal128 { boost::decimal::detail::uint128 { UINT64_C(251343872473191), UINT64_C(15780610568723885484) }, -34 }, // atan_half
-        ::boost::decimal::decimal128 { boost::decimal::detail::uint128 { UINT64_C(425765197510819), UINT64_C(5970600460659265246)  }, -34 }, // atan_one
-        ::boost::decimal::decimal128 { boost::decimal::detail::uint128 { UINT64_C(532773544924935), UINT64_C(16408933314882201700) }, -34 }, // atan_three_halves
-    }};
-
     // 10th degree remez polynomial calculated from 0, 0.4375
     // Estimated max error: 2.3032664387910605e-12
     static constexpr std::array<::boost::decimal::decimal32, 11> d32_coeffs =
