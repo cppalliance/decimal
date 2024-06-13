70b1b2d4013d6dcf09672c106c47f2c6b8cf698f
Matt Borland <matt@mattborland.com>
2024-06-13 09:06:21 -0400

    Add same workaround from decimal32_fast

--- test/test_decimal64_fast_basis.cpp
+++ test/test_decimal64_fast_basis.cpp
@@ -412,7 +412,7 @@ int main()
     test_construct_from_float<float>();
     test_construct_from_float<double>();
     test_construct_from_float<long double>();
-    #ifdef BOOST_DECIMAL_HAS_FLOAT128
+    #if defined(BOOST_DECIMAL_HAS_FLOAT128) && (!defined(__clang_major__) || __clang_major__ >= 13)
     test_construct_from_float<__float128>();
     #endif
 
