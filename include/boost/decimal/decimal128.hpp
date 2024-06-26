83e28d066bc7f226036623a8c2eb333a161defa6
Matt Borland <matt@mattborland.com>
2024-06-26 11:44:58 -0400

    Remove duplicate ops from operator+

--- include/boost/decimal/decimal128.hpp
+++ include/boost/decimal/decimal128.hpp
@@ -1565,17 +1565,16 @@ constexpr auto operator+(decimal128 lhs, decimal128 rhs) noexcept -> decimal128
               << "\nrhs exp: " << rhs_exp << std::endl;
     #endif
 
-    const auto result {detail::d128_add_impl<detail::decimal128_components>(
-                                     lhs_sig, lhs_exp, lhs.isneg(),
-                                     rhs_sig, rhs_exp, rhs.isneg())};
-
-    return {result.sig, result.exp, result.sign};
+    return {detail::d128_add_impl<decimal128>(lhs_sig, lhs_exp, lhs.isneg(),
+                                              rhs_sig, rhs_exp, rhs.isneg())};
 }
 
 template <typename Integer>
 constexpr auto operator+(decimal128 lhs, Integer rhs) noexcept
     BOOST_DECIMAL_REQUIRES_RETURN(detail::is_integral_v, Integer, decimal128)
 {
+    using exp_type = decimal128::biased_exponent_type;
+
     #ifndef BOOST_DECIMAL_FAST_MATH
     if (isnan(lhs) || isinf(lhs))
     {
@@ -1588,18 +1587,17 @@ constexpr auto operator+(decimal128 lhs, Integer rhs) noexcept
     {
         lhs_bigger = !lhs_bigger;
     }
-    bool abs_lhs_bigger {abs(lhs) > detail::make_positive_unsigned(rhs)};
+    auto sig_rhs {static_cast<detail::uint128>(detail::make_positive_unsigned(rhs))};
+    bool abs_lhs_bigger {abs(lhs) > sig_rhs};
 
     auto sig_lhs {lhs.full_significand()};
     auto exp_lhs {lhs.biased_exponent()};
     detail::normalize<decimal128>(sig_lhs, exp_lhs);
     auto lhs_components {detail::decimal128_components{sig_lhs, exp_lhs, lhs.isneg()}};
 
-    auto sig_rhs {static_cast<detail::uint128>(detail::make_positive_unsigned(rhs))};
-    std::int32_t exp_rhs {0};
+    exp_type exp_rhs {0};
     detail::normalize<decimal128>(sig_rhs, exp_rhs);
-    auto unsigned_sig_rhs = detail::make_positive_unsigned(sig_rhs);
-    auto rhs_components {detail::decimal128_components{unsigned_sig_rhs, exp_rhs, (rhs < 0)}};
+    auto rhs_components {detail::decimal128_components{sig_rhs, exp_rhs, (rhs < 0)}};
 
     if (!lhs_bigger)
     {
@@ -1607,8 +1605,6 @@ constexpr auto operator+(decimal128 lhs, Integer rhs) noexcept
         lhs_bigger = !lhs_bigger;
         abs_lhs_bigger = !abs_lhs_bigger;
     }
-
-    detail::decimal128_components result {};
 
     #ifdef BOOST_DECIMAL_DEBUG_ADD
     std::cerr << "Lhs sig: " << lhs_components.sig
@@ -1619,19 +1615,17 @@ constexpr auto operator+(decimal128 lhs, Integer rhs) noexcept
 
     if (!lhs_components.sign && rhs_components.sign)
     {
-        result = detail::d128_sub_impl<detail::decimal128_components>(
+        return detail::d128_sub_impl<decimal128>(
                 lhs_components.sig, lhs_components.exp, lhs_components.sign,
                 rhs_components.sig, rhs_components.exp, rhs_components.sign,
                 abs_lhs_bigger);
     }
     else
     {
-        result = detail::d128_add_impl<detail::decimal128_components>(
+        return detail::d128_add_impl<decimal128>(
                 lhs_components.sig, lhs_components.exp, lhs_components.sign,
                 rhs_components.sig, rhs_components.exp, rhs_components.sign);
     }
-
-    return decimal128(result.sig, result.exp, result.sign);
 }
 
 template <typename Integer>
