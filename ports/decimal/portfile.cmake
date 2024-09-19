5549c3778a3a2dc8cacae179a4e1f68106791418
Matt Borland <matt@mattborland.com>
2024-09-19 10:07:28 -0400

    Update packages

--- ports/decimal/portfile.cmake
+++ ports/decimal/portfile.cmake
@@ -7,8 +7,8 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO cppalliance/decimal
-        REF v1.1.0
-        SHA512 9ee10a32958a58e96ec4d1fc5f39e3c86352d36365547716e7903340435878b86b56f74e8abcaedfc227154c409d8ccfdb0563a2d98b605aa1799c071ba15cca
+        REF v2.3.0
+        SHA512 a5dac21e3f786e028b5e526c615c1ff7f671f2759a6968edaf97de90f5c5ede59e956dca730d14edabce4d86b58d5dacca2dab0706cb9e17474871f6433171eb
         HEAD_REF master
 )
 
