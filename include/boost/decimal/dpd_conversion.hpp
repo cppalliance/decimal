c11da26c9e8f9ee1639a7386c2360067f9b1e223
Matt Borland <matt@mattborland.com>
2024-09-26 10:34:43 -0400

    Implement proper encoding

--- include/boost/decimal/dpd_conversion.hpp
+++ include/boost/decimal/dpd_conversion.hpp
@@ -245,7 +245,95 @@ BOOST_DECIMAL_CXX20_CONSTEXPR auto to_dpd_d32(DecimalType val) noexcept
         return to_bid(val);
     }
 
-    return 0U;
+    const auto sign {val.isneg()};
+    const auto exp {val.unbiased_exponent()};
+    const auto significand {val.full_significand()};
+
+    std::uint32_t dpd {};
+    // Set the sign bit as applicable
+    if (sign)
+    {
+        dpd |= detail::d32_sign_mask;
+    }
+
+    // Break the significand down into the 7 declets are needed
+    std::uint8_t d[std::numeric_limits<DecimalType>::digits10];
+    auto temp_sig {significand};
+    for (int i = 7; i >= 0; ++i)
+    {
+        d[i] = temp_sig % 10;
+        temp_sig /= 10;
+    }
+    BOOST_DECIMAL_ASSERT(d[0] >= 0 && d[0] <= 9);
+
+    // We now need to capture what the leading two bits of the exponent are,
+    // since they are stored in the combination field
+    constexpr std::uint32_t leading_two_exp_bits_mask {0b11000000};
+    const auto leading_two_bits {(exp & leading_two_exp_bits_mask) >> 6U};
+    BOOST_DECIMAL_ASSERT(leading_two_bits >= 0 && leading_two_bits <= 2);
+    constexpr std::uint32_t trailing_exp_bits_mask {0b00111111};
+    const auto trailing_exp_bits {(exp & trailing_exp_bits_mask)};
+
+
+    std::uint32_t combination_field_bits {};
+    // Now based on what the value of d[0] and the leading bits of exp are we can set the value of the combination field
+    // See 3.5.2.c.1
+    // If d0 is 8 or 9 then we follow section i
+    if (d[0] >= 8)
+    {
+        const auto d0_is_nine {d[0] == 9};
+        switch (leading_two_bits)
+        {
+            case 0U:
+                combination_field_bits = d0_is_nine ? 0b11001 : 0b11000;
+                break;
+            case 1U:
+                combination_field_bits = d0_is_nine ? 0b11011 : 0b11010;
+                break;
+            case 2U:
+                combination_field_bits = d0_is_nine ? 0b11101 : 0b11100;
+                break;
+            default:
+                BOOST_DECIMAL_UNREACHABLE;
+        }
+    }
+    // If d0 is 0 to 7 then we follow section II
+    else
+    {
+        // In here the value of d[0] = 4*G2 + 2*G3 + G4
+        const auto d0_mask {static_cast<std::uint32_t>(d[0])};
+        switch (leading_two_bits)
+        {
+            case 0U:
+                // 00XXX
+                combination_field_bits |= d0_mask;
+                break;
+            case 1U:
+                // 01XXX
+                combination_field_bits = 0b01000;
+                combination_field_bits |= d0_mask;
+                break;
+            case 2U:
+                // 10XXX
+                combination_field_bits = 0b10000;
+                combination_field_bits |= d0_mask;
+                break;
+            default:
+                BOOST_DECIMAL_UNREACHABLE;
+        }
+    }
+
+    // The only thing we have left to compute now is the bit patterns of d[1] - d[6]
+    const auto declet_1 {static_cast<std::uint32_t>(detail::encode_dpd(d[1], d[2], d[3]))};
+    const auto declet_2 {static_cast<std::uint32_t>(detail::encode_dpd(d[4], d[5], d[6]))};
+
+    // Now we can do final assembly of the number
+    dpd |= (combination_field_bits << 26U);
+    dpd |= (trailing_exp_bits << 20U);
+    dpd |= (declet_1 << 10U);
+    dpd |= declet_2;
+
+    return dpd;
 }
 
 BOOST_DECIMAL_CXX20_CONSTEXPR auto to_dpd(decimal32 val) noexcept -> std::uint32_t
