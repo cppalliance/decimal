ff910130f4a8e413c32977abc8521c6e93589134
Matt Borland <matt@mattborland.com>
2025-01-07 11:13:56 -0500

    Add format example

---
+++ examples/format.cpp
@@ -0,0 +1,31 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/decimal.hpp>
+#include <iostream>
+
+#ifdef BOOST_CRYPT_HAS_FORMAT_SUPPORT
+
+#include <format>
+
+int main()
+{
+    constexpr boost::decimal::decimal64 val1 {314, -2};
+    constexpr boost::decimal::decimal32 val2 {3141, -3};
+
+    std::cout << std::format("{:10.3e}", val1) << '\n';
+    std::cout << std::format("{:10.3e}", val2) << std::endl;
+
+    return 0;
+}
+
+#else
+
+int main()
+{
+    std::cout << "<format> is unsupported" << std::endl;
+    return 0;
+}
+
+#endif
