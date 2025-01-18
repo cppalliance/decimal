6675c29927d4f195b8141293dec02c67967ecf0d
Matt Borland <matt@mattborland.com>
2025-01-18 11:34:19 -0500

    Add decimal32(_fast) limits printing tests

---
+++ test/test_limits.cpp
@@ -0,0 +1,40 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/decimal.hpp>
+#include <boost/core/lightweight_test.hpp>
+
+using namespace boost::decimal;
+
+template <typename T>
+void test_value(T value, const char* res)
+{
+    char buffer[256];
+    const auto r = to_chars(buffer, buffer + sizeof(buffer), value, chars_format::scientific, 30);
+    *r.ptr = '\0';
+    BOOST_TEST(r);
+    BOOST_TEST_CSTR_EQ(buffer, res);
+}
+
+int main()
+{
+    // Max
+    test_value(std::numeric_limits<decimal32>::max(), "9.999999000000000000000000000000e+96");
+    test_value(std::numeric_limits<decimal32_fast>::max(), "9.999999000000000000000000000000e+96");
+
+    // Epsilon
+    test_value(std::numeric_limits<decimal32>::epsilon(), "1.000000000000000000000000000000e-06");
+    test_value(std::numeric_limits<decimal32_fast>::epsilon(), "1.000000000000000000000000000000e-06");
+
+    // Min
+    test_value(std::numeric_limits<decimal32>::min(), "1.000000000000000000000000000000e-95");
+    test_value(std::numeric_limits<decimal32_fast>::min(), "1.000000000000000000000000000000e-95");
+
+    // Min subnormal
+    // Fast types don't support sub-normals so they should return min
+    test_value(std::numeric_limits<decimal32>::denorm_min(), "1.000000000000000000000000000000e-101");
+    test_value(std::numeric_limits<decimal32_fast>::denorm_min(), "1.000000000000000000000000000000e-95");
+
+    return boost::report_errors();
+}
