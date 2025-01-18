d3a54da02aafaac4ea4eacdf516a261d5147c91d
Matt Borland <matt@mattborland.com>
2025-01-18 12:13:32 -0500

    Add consteval testing to ensure these are properly constructed

--- test/test_limits.cpp
+++ test/test_limits.cpp
@@ -5,6 +5,70 @@
 #include <boost/decimal.hpp>
 #include <boost/core/lightweight_test.hpp>
 
+#if defined(__cpp_consteval) && __cpp_consteval >= 201811L && !defined(BOOST_DECIMAL_NO_CONSTEVAL_DETECTION)
+
+using namespace boost::decimal;
+
+template <typename T>
+consteval bool test_value(T value, const char* res)
+{
+    char buffer[256];
+    const auto r = to_chars(buffer, buffer + sizeof(buffer), value, chars_format::scientific, 40);
+    *r.ptr = '\0';
+    
+    auto res_ptr {res};
+    auto buffer_ptr {buffer};
+    while (*buffer_ptr != '\0')
+    {
+        if (*buffer_ptr++ != *res_ptr++)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main()
+{
+    // Max
+    static_assert(test_value(std::numeric_limits<decimal32>::max(), "9.9999990000000000000000000000000000000000e+96"));
+    static_assert(test_value(std::numeric_limits<decimal32_fast>::max(), "9.9999990000000000000000000000000000000000e+96"));
+    static_assert(test_value(std::numeric_limits<decimal64>::max(), "9.9999999999999990000000000000000000000000e+384"));
+    static_assert(test_value(std::numeric_limits<decimal64_fast>::max(), "9.9999999999999990000000000000000000000000e+384"));
+    static_assert(test_value(std::numeric_limits<decimal128>::max(), "9.9999999999999999999999999999999990000000e+6144"));
+    static_assert(test_value(std::numeric_limits<decimal128_fast>::max(), "9.9999999999999999999999999999999990000000e+6144"));
+
+    // Epsilon
+    static_assert(test_value(std::numeric_limits<decimal32>::epsilon(), "1.0000000000000000000000000000000000000000e-06"));
+    static_assert(test_value(std::numeric_limits<decimal32_fast>::epsilon(), "1.0000000000000000000000000000000000000000e-06"));
+    static_assert(test_value(std::numeric_limits<decimal64>::epsilon(), "1.0000000000000000000000000000000000000000e-15"));
+    static_assert(test_value(std::numeric_limits<decimal64_fast>::epsilon(), "1.0000000000000000000000000000000000000000e-15"));
+    static_assert(test_value(std::numeric_limits<decimal128>::epsilon(), "1.0000000000000000000000000000000000000000e-33"));
+    static_assert(test_value(std::numeric_limits<decimal128_fast>::epsilon(), "1.0000000000000000000000000000000000000000e-33"));
+
+    // Min
+    static_assert(test_value(std::numeric_limits<decimal32>::min(), "1.0000000000000000000000000000000000000000e-95"));
+    static_assert(test_value(std::numeric_limits<decimal32_fast>::min(), "1.0000000000000000000000000000000000000000e-95"));
+    static_assert(test_value(std::numeric_limits<decimal64>::min(), "1.0000000000000000000000000000000000000000e-383"));
+    static_assert(test_value(std::numeric_limits<decimal64_fast>::min(), "1.0000000000000000000000000000000000000000e-383"));
+    static_assert(test_value(std::numeric_limits<decimal128>::epsilon(), "1.0000000000000000000000000000000000000000e-33"));
+    static_assert(test_value(std::numeric_limits<decimal128_fast>::epsilon(), "1.0000000000000000000000000000000000000000e-33"));
+
+    // Min subnormal
+    // Fast types don't support sub-normals so they should return min
+    static_assert(test_value(std::numeric_limits<decimal32>::denorm_min(), "1.0000000000000000000000000000000000000000e-101"));
+    static_assert(test_value(std::numeric_limits<decimal32_fast>::denorm_min(), "1.0000000000000000000000000000000000000000e-95"));
+    static_assert(test_value(std::numeric_limits<decimal64>::denorm_min(), "1.0000000000000000000000000000000000000000e-398"));
+    static_assert(test_value(std::numeric_limits<decimal64_fast>::denorm_min(), "1.0000000000000000000000000000000000000000e-383"));
+    static_assert(test_value(std::numeric_limits<decimal128>::denorm_min(), "1.0000000000000000000000000000000000000000e-6176"));
+    static_assert(test_value(std::numeric_limits<decimal128_fast>::denorm_min(), "1.0000000000000000000000000000000000000000e-6143"));
+
+    return boost::report_errors();
+}
+
+#else
+
 using namespace boost::decimal;
 
 template <typename T>
@@ -54,3 +118,5 @@ int main()
 
     return boost::report_errors();
 }
+
+#endif
