9c3ec0fc8a42a53e304522c3ff9ad43db18d04a6
Matt Borland <matt@mattborland.com>
2025-01-28 14:40:01 -0500

    Update basic compile test

--- test/compile_tests/gcc_decimal32.cpp
+++ test/compile_tests/gcc_decimal32.cpp
@@ -2,7 +2,9 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#if defined(__GNUC__) && __GNUC__ >= 5
+#include <boost/decimal/detail/config.hpp>
+
+#ifdef BOOST_DECIMAL_HAS_LIBSTDCPP_DECIMAL
 
 #include <boost/decimal/gcc_decimal32.hpp>
 
