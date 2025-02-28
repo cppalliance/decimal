508ce87f56a5fcc3f80f3dc4689fb1821ab3548c
Matt Borland <matt@mattborland.com>
2025-02-28 11:20:56 -0500

    Add bool and integer conversion operators

--- include/boost/decimal/detail/u128.hpp
+++ include/boost/decimal/detail/u128.hpp
@@ -85,6 +85,29 @@ public:
     #ifdef BOOST_DECIMAL_HAS_INT128
     constexpr u128& operator=(const unsigned __int128 value) noexcept { low = static_cast<std::uint64_t>(value & low_word_mask); high = static_cast<std::uint64_t>(value >> 64U); return *this; }
     #endif
+
+    // Bool conversion is not explicit so we can do stuff like if (num)
+    constexpr operator bool() const noexcept { return low || high; }
+
+    // Conversion to signed integer types
+    explicit constexpr operator std::int8_t() const noexcept { return static_cast<std::int8_t>(low); }
+    explicit constexpr operator std::int16_t() const noexcept { return static_cast<std::int16_t>(low); }
+    explicit constexpr operator std::int32_t() const noexcept { return static_cast<std::int32_t>(low); }
+    explicit constexpr operator std::int64_t() const noexcept { return static_cast<std::int64_t>(low); }
+
+    #ifdef BOOST_DECIMAL_HAS_INT128
+    explicit constexpr operator __int128() const noexcept { return (static_cast<__int128>(high) << 64) + low; }
+    #endif // BOOST_DECIMAL_HAS_INT128
+
+    // Conversion to unsigned integer types
+    explicit constexpr operator std::uint8_t() const noexcept { return static_cast<std::uint8_t>(low); }
+    explicit constexpr operator std::uint16_t() const noexcept { return static_cast<std::uint16_t>(low); }
+    explicit constexpr operator std::uint32_t() const noexcept { return static_cast<std::uint32_t>(low); }
+    explicit constexpr operator std::uint64_t() const noexcept { return low; }
+
+    #ifdef BOOST_DECIMAL_HAS_INT128
+    explicit constexpr operator unsigned __int128() const noexcept { return (static_cast<unsigned __int128>(high) << 64U) + low; }
+    #endif // BOOST_DECIMAL_HAS_INT128
 };
 
 } // namespace detail
