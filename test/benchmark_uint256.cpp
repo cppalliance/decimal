f41c5709df08cd196bb80684827554dabf74240b
Matt Borland <matt@mattborland.com>
2025-03-24 15:04:55 -0400

    Add and benchmark

--- test/benchmark_uint256.cpp
+++ test/benchmark_uint256.cpp
@@ -21,7 +21,7 @@
 #include <cstring>
 #include <functional>
 
-constexpr unsigned N = 20'000'000;
+constexpr unsigned N = 200'000'000;
 constexpr unsigned K = 5;
 
 using namespace boost::decimal;
@@ -150,6 +150,7 @@ BOOST_DECIMAL_NO_INLINE void test_bitwise_ops(const std::vector<T>& data_vec, co
             const auto val1 = data_vec[i];
             const auto val2 = data_vec[i + 1];
             s += static_cast<std::size_t>(val1 | val2);
+            s += static_cast<std::size_t>(val1 & val2);
         }
     }
 
