e56cd5fa95b17b152ac631327626e9f7d7203500
Matt Borland <matt@mattborland.com>
2025-03-24 15:35:29 -0400

    Add test for operator and

--- test/test_u256.cpp
+++ test/test_u256.cpp
@@ -824,6 +824,163 @@ void test_or()
     }
 }
 
+void test_and()
+{
+    std::uniform_int_distribution<std::uint64_t> dist(0, 0xFFFFFFFFFFFFFFFFULL);
+
+    // one word
+    for (std::size_t i {}; i < N; ++i)
+    {
+        std::uint64_t num = dist(rng);
+        u256 new_val_1;
+        new_val_1.bytes[0] = num;
+        uint256_t old_val_1;
+        old_val_1.low.low = num;
+
+        auto new_res {new_val_1 & new_val_1};
+        auto old_res {old_val_1 & old_val_1};
+
+        BOOST_TEST(new_res[3] == old_res.high.high);
+        BOOST_TEST(new_res[2] == old_res.high.low);
+        BOOST_TEST(new_res[1] == old_res.low.high);
+        BOOST_TEST(new_res[0] == old_res.low.low);
+
+        num = dist(rng);
+        u256 new_val_2;
+        new_val_2.bytes[0] = num;
+        uint256_t old_val_2;
+        old_val_2.low.low = num;
+
+        new_res = new_val_2 & new_val_1;
+        old_res = old_val_2 & old_val_1;
+
+        BOOST_TEST(new_res[3] == old_res.high.high);
+        BOOST_TEST(new_res[2] == old_res.high.low);
+        BOOST_TEST(new_res[1] == old_res.low.high);
+        BOOST_TEST(new_res[0] == old_res.low.low);
+    }
+
+    // two word
+    for (std::size_t i {}; i < N; ++i)
+    {
+        std::uint64_t num = dist(rng);
+        u256 new_val_1;
+        new_val_1.bytes[0] = num;
+        new_val_1.bytes[1] = num;
+        uint256_t old_val_1;
+        old_val_1.low.low = num;
+        old_val_1.low.high = num;
+
+        auto new_res {new_val_1 & new_val_1};
+        auto old_res {old_val_1 & old_val_1};
+
+        BOOST_TEST(new_res[3] == old_res.high.high);
+        BOOST_TEST(new_res[2] == old_res.high.low);
+        BOOST_TEST(new_res[1] == old_res.low.high);
+        BOOST_TEST(new_res[0] == old_res.low.low);
+
+        num = dist(rng);
+        u256 new_val_2;
+        new_val_2.bytes[0] = num;
+        new_val_2.bytes[1] = num;
+        uint256_t old_val_2;
+        old_val_2.low.low = num;
+        old_val_2.low.high = num;
+
+        new_res = new_val_2 & new_val_1;
+        old_res = old_val_2 & old_val_1;
+
+        BOOST_TEST(new_res[3] == old_res.high.high);
+        BOOST_TEST(new_res[2] == old_res.high.low);
+        BOOST_TEST(new_res[1] == old_res.low.high);
+        BOOST_TEST(new_res[0] == old_res.low.low);
+    }
+
+    // three word
+    for (std::size_t i {}; i < N; ++i)
+    {
+        std::uint64_t num = dist(rng);
+        u256 new_val_1;
+        new_val_1.bytes[0] = num;
+        new_val_1.bytes[1] = num;
+        new_val_1.bytes[2] = num;
+        uint256_t old_val_1;
+        old_val_1.low.low = num;
+        old_val_1.low.high = num;
+        old_val_1.high.low = num;
+
+        auto new_res {new_val_1 & new_val_1};
+        auto old_res {old_val_1 & old_val_1};
+
+        BOOST_TEST(new_res[3] == old_res.high.high);
+        BOOST_TEST(new_res[2] == old_res.high.low);
+        BOOST_TEST(new_res[1] == old_res.low.high);
+        BOOST_TEST(new_res[0] == old_res.low.low);
+
+        num = dist(rng);
+        u256 new_val_2;
+        new_val_2.bytes[0] = num;
+        new_val_2.bytes[1] = num;
+        new_val_2.bytes[2] = num;
+        uint256_t old_val_2;
+        old_val_2.low.low = num;
+        old_val_2.low.high = num;
+        old_val_2.high.low = num;
+
+        new_res = new_val_2 & new_val_1;
+        old_res = old_val_2 & old_val_1;
+
+        BOOST_TEST(new_res[3] == old_res.high.high);
+        BOOST_TEST(new_res[2] == old_res.high.low);
+        BOOST_TEST(new_res[1] == old_res.low.high);
+        BOOST_TEST(new_res[0] == old_res.low.low);
+    }
+
+    // four word
+    for (std::size_t i {}; i < N; ++i)
+    {
+        std::uint64_t num = dist(rng);
+        u256 new_val_1;
+        new_val_1.bytes[0] = num;
+        new_val_1.bytes[1] = num;
+        new_val_1.bytes[2] = num;
+        new_val_1.bytes[3] = num;
+        uint256_t old_val_1;
+        old_val_1.low.low = num;
+        old_val_1.low.high = num;
+        old_val_1.high.low = num;
+        old_val_1.high.high = num;
+
+        auto new_res {new_val_1 & new_val_1};
+        auto old_res {old_val_1 & old_val_1};
+
+        BOOST_TEST(new_res[3] == old_res.high.high);
+        BOOST_TEST(new_res[2] == old_res.high.low);
+        BOOST_TEST(new_res[1] == old_res.low.high);
+        BOOST_TEST(new_res[0] == old_res.low.low);
+
+        num = dist(rng);
+        u256 new_val_2;
+        new_val_2.bytes[0] = num;
+        new_val_2.bytes[1] = num;
+        new_val_2.bytes[2] = num;
+        new_val_2.bytes[3] = num;
+        uint256_t old_val_2;
+        old_val_2.low.low = num;
+        old_val_2.low.high = num;
+        old_val_2.high.low = num;
+        old_val_2.high.high = num;
+
+        new_res = new_val_2 & new_val_1;
+        old_res = old_val_2 & old_val_1;
+
+        BOOST_TEST(new_res[3] == old_res.high.high);
+        BOOST_TEST(new_res[2] == old_res.high.low);
+        BOOST_TEST(new_res[1] == old_res.low.high);
+        BOOST_TEST(new_res[0] == old_res.low.low);
+    }
+}
+
 int main()
 {
     test_equality();
@@ -833,6 +990,7 @@ int main()
     test_greater();
 
     test_or();
+    test_and();
 
     return boost::report_errors();
 }
