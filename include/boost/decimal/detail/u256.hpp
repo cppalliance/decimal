0a88cff542e9218b8a1a611a2e8f87927c0d765b
Matt Borland <matt@mattborland.com>
2025-06-26 11:48:33 -0400

    Add subscript operators

--- include/boost/decimal/detail/u256.hpp
+++ include/boost/decimal/detail/u256.hpp
@@ -1,6 +1,9 @@
 // Copyright 2023 - 2025 Matt Borland
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
+//
+// This is not a fully featured implementation of a 256-bit integer like int128::uint128_t is
+// u256 only contains the minimum amount that we need to perform operations like decimal128 mul
 
 #ifndef BOOST_DECIMAL_DETAIL_U256_HPP
 #define BOOST_DECIMAL_DETAIL_U256_HPP
@@ -34,8 +37,12 @@ u256
 
     // Conversion to/from int128::uint128_t
     constexpr u256(const int128::uint128_t& high_, const int128::uint128_t& low_) noexcept;
-
     explicit constexpr operator int128::uint128_t() const noexcept;
+
+    explicit constexpr operator std::size_t() const noexcept { return static_cast<std::size_t>(bytes[0]); }
+
+    constexpr std::uint64_t operator[](std::size_t i) const noexcept;
+    constexpr std::uint64_t& operator[](std::size_t i) noexcept;
 };
 
 constexpr u256::u256(const int128::uint128_t& high_, const int128::uint128_t& low_) noexcept
@@ -51,6 +58,17 @@ constexpr u256::operator int128::uint128_t() const noexcept
     return int128::uint128_t {bytes[1], bytes[0]};
 }
 
+constexpr std::uint64_t u256::operator[](const std::size_t i) const noexcept
+{
+    BOOST_DECIMAL_ASSERT(i < 4);
+    return bytes[i];
+}
+
+constexpr std::uint64_t& u256::operator[](const std::size_t i) noexcept
+{
+    BOOST_DECIMAL_ASSERT(i < 4);
+    return bytes[i];
+}
 
 } // namespace detail
 } // namespace decimal
