be29ee2355d460e224f1eafb91e7b37ab47de3ee
Matt Borland <matt@mattborland.com>
2025-07-07 16:40:39 -0400

    Add 64 bit test set

--- test/github_issue_893.cpp
+++ test/github_issue_893.cpp
@@ -8,6 +8,24 @@
 #include <boost/core/lightweight_test.hpp>
 #include <array>
 
+void test_64()
+{
+    const std::array<std::uint64_t, 6> comp_values = {
+        UINT64_C(12345678901234567),
+        UINT64_C(123456789012345678),
+        UINT64_C(1234567890123456789),
+        UINT64_C(12345678901234567890),
+    };
+
+    int res {17};
+
+    for (const auto& value : comp_values)
+    {
+        BOOST_TEST_EQ(boost::decimal::detail::num_digits(value), res);
+        BOOST_TEST_EQ(boost::decimal::detail::d64_constructor_num_digits(value), res++);
+    }
+}
+
 void test_128()
 {
     const std::array<boost::int128::uint128_t, 6> comp_values = {
@@ -40,6 +58,7 @@ void test_128()
 
 int main()
 {
+    test_64();
     test_128();
 
     return boost::report_errors();
