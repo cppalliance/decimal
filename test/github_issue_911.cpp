641e157a2e99fad311ac11eba75cc1eab4bdfe8a
Matt Borland <matt@mattborland.com>
2025-07-15 14:04:11 -0400

    Add random value testing against bson

--- test/github_issue_911.cpp
+++ test/github_issue_911.cpp
@@ -8,7 +8,7 @@
 #include <boost/core/lightweight_test.hpp>
 #include <cstring>
 
-int main()
+void test_issue()
 {
     // -7.50
     constexpr boost::decimal::decimal128 test_val {-750, -2};
@@ -17,6 +17,64 @@ int main()
 
     BOOST_TEST_EQ(bits.low, UINT64_C(750));
     BOOST_TEST_EQ(bits.high, UINT64_C(12699025049277956096));
+}
+
+#if __has_include(<bson-2.0.2/bson/bson.h>)
+
+#include <bson-2.0.2/bson/bson.h>
+#include <random>
+#include <string>
+
+void test_random_spots()
+{
+    std::mt19937_64 rng(42);
+    std::uniform_int_distribution<int> integer_part_dist(INT_MIN, INT_MAX);
+    std::uniform_int_distribution<std::uint64_t> fractional_part_dist(UINT64_C(0), UINT64_MAX);
+
+    for (std::size_t i {}; i < 1024; ++i)
+    {
+        const auto integer_part {integer_part_dist(rng)};
+        const auto fractional_part {fractional_part_dist(rng)};
+
+        const auto integer_string {std::to_string(integer_part)};
+        const auto fractional_string {std::to_string(fractional_part)};
+
+        const auto full_string {integer_string + "." + fractional_string};
+
+        bson_decimal128_t bson_res;
+        bson_decimal128_from_string(full_string.c_str(), &bson_res);
+
+        boost::decimal::decimal128 boost_res;
+        const auto r = boost::decimal::from_chars(full_string, boost_res);
+
+        BOOST_TEST(r);
+
+        boost::int128::uint128_t bson_bits;
+        std::memcpy(&bson_bits, &boost_res, sizeof(bson_bits));
+
+        boost::int128::uint128_t boost_bits;
+        std::memcpy(&boost_bits, &bson_bits, sizeof(boost_bits));
+
+        BOOST_TEST_EQ(bson_bits.low, boost_bits.low);
+        BOOST_TEST_EQ(bson_bits.high, boost_bits.high);
+    }
+}
+
+int main()
+{
+    test_issue();
+    test_random_spots();
 
     return boost::report_errors();
 }
+
+#else
+
+int main()
+{
+    test_issue();
+
+    return boost::report_errors();
+}
+
+#endif
