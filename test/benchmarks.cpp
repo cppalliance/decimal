deea1e0a571d4aac891b59f97d84c573acbbf8a5
Matt Borland <matt@mattborland.com>
2025-07-21 14:19:18 -0400

    Update benchmark macro

--- test/benchmarks.cpp
+++ test/benchmarks.cpp
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#define BOOST_INT128_ALLOW_SIGN_CONVERSION
+#define BOOST_DECIMAL_DETAIL_INT128_ALLOW_SIGN_CONVERSION
 
 #include <boost/decimal.hpp>
 #include <chrono>
