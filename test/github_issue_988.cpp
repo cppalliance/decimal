6ffa653604849110bcf3481d23c841dd210ba025
Matt Borland <matt@mattborland.com>
2025-08-13 16:44:57 +0200

    Update test set

--- test/github_issue_988.cpp
+++ test/github_issue_988.cpp
@@ -7,25 +7,35 @@
 #include <boost/decimal.hpp>
 #include <boost/core/lightweight_test.hpp>
 
+using namespace boost::decimal;
+
 constexpr std::uint64_t value {0xb0005af3107a3ff0};
+constexpr decimal64_t decimal_value {from_bits(value)};
+
 
 void test()
 {
-    using namespace boost::decimal;
-
-    constexpr decimal64_t decimal_value {boost::decimal::from_bits(value)};
-
     // Regular to chars
     char buffer[256];
     const auto r = to_chars(buffer, buffer + sizeof(buffer), decimal_value);
     BOOST_TEST(r);
     *r.ptr = '\0';
-    std::cerr << buffer << '\n';
+    BOOST_TEST_CSTR_EQ(buffer, "-9.9999999999984e-01");
+}
+
+void fixed_precision_test()
+{
+    char buffer[256];
+    const auto r = to_chars(buffer, buffer + sizeof(buffer), decimal_value, chars_format::general, 6);
+    BOOST_TEST(r);
+    *r.ptr = '\0';
+    BOOST_TEST_CSTR_EQ(buffer, "-1");
 }
 
 int main()
 {
     test();
+    fixed_precision_test();
 
     return boost::report_errors();
 }
