b747b3505db674d2c496501d47996734f389ffbb
Matt Borland <matt@mattborland.com>
2025-09-04 09:18:04 +0200

    Add test set from issue

---
+++ test/github_issue_1026.cpp
@@ -0,0 +1,28 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+//
+// See: https://github.com/cppalliance/decimal/issues/1026
+
+#include <boost/decimal.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include <cstdint>
+
+using namespace boost::decimal;
+using namespace boost::decimal::literals;
+
+int main()
+{
+    // Round ties to even
+
+    BOOST_TEST_EQ("1234567.49"_DF, "1234567"_DF);
+    BOOST_TEST_EQ("1234567.50"_DF, "1234568"_DF);
+    BOOST_TEST_EQ("1234567.51"_DF, "1234568"_DF);
+
+    BOOST_TEST_EQ("2345678.49"_DF, "2345678"_DF);
+    BOOST_TEST_EQ("2345678.50"_DF, "2345678"_DF);
+    BOOST_TEST_EQ("2345678.51"_DF, "2345679"_DF);
+
+    return boost::report_errors();
+}
+
