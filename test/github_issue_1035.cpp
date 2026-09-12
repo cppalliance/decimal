bad17ca9dbdae550ccb4b51bd8ec52ce53e98c55
Shen-Ta Hsieh(BestSteve) <ibmibmibm.tw@gmail.com>
2026-09-13 01:37:52 +0800

    Round a sum of far apart operands by the sign of the result (#1452)

--- test/github_issue_1035.cpp
+++ test/github_issue_1035.cpp
@@ -34,14 +34,14 @@ int main()
     fesetround(rounding_mode::fe_dec_downward);
     BOOST_TEST_EQ("5e+50"_DF * dist(rng) - "4e+40"_DF, "4.999999e+50"_DF);
     BOOST_TEST_EQ("5e+95"_DF * dist(rng)  - "4e-100"_DF, "4.999999e+95"_DF);
-    BOOST_TEST_EQ("-5e+95"_DF * dist(rng)  + "4e-100"_DF, "-4.999999e+95"_DF);
-    BOOST_TEST_EQ("-5e+95"_DL * dist(rng)  + "4e-100"_DL, "-4.999999999999999999999999999999999e+95"_DL);
+    BOOST_TEST_EQ("-5e+95"_DF * dist(rng)  + "4e-100"_DF, "-5e+95"_DF);
+    BOOST_TEST_EQ("-5e+95"_DL * dist(rng)  + "4e-100"_DL, "-5e+95"_DL);
 
     fesetround(rounding_mode::fe_dec_upward);
     BOOST_TEST_EQ("5e+50"_DF * dist(rng) + "4e+40"_DF, "5.000001e+50"_DF);
     BOOST_TEST_EQ("5e+95"_DF * dist(rng)  + "4e-100"_DF, "5.000001e+95"_DF);
-    BOOST_TEST_EQ("-5e+95"_DF * dist(rng)  - "4e-100"_DF, "-5.000001e+95"_DF);
-    BOOST_TEST_EQ("-5e+95"_DL * dist(rng)  - "4e-100"_DL, "-5.000000000000000000000000000000001e+95"_DL);
+    BOOST_TEST_EQ("-5e+95"_DF * dist(rng)  - "4e-100"_DF, "-5e+95"_DF);
+    BOOST_TEST_EQ("-5e+95"_DL * dist(rng)  - "4e-100"_DL, "-5e+95"_DL);
 
     return boost::report_errors();
 
